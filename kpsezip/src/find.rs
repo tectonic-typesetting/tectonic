e5c2d02aa344168224e5b8142ea70f3238b65cc3
Peter Williams <peter@newton.cx>
2016-11-19 16:05:26 -0500

    kpsezip/src/find.rs: also try adding extensions when searching in the bundle
    Took me a bit how to figure out how to do this correctly. But once I figured it out ...
    Rust is really cool!

--- kpsezip/src/find.rs
+++ kpsezip/src/find.rs
@@ -51,6 +51,22 @@ impl<R: Read + Seek> FinderState<R> {
         )
     }
 
+    fn zip_to_temp_fd (&mut self, name: &Path) -> Result<RawFd,ZipError> {
+        let mut zipitem = match self.zip.by_name (name.to_str ().unwrap ()) {
+            Err(e) => return Err(e),
+            Ok(f) => f
+        };
+
+        let temp_file = Temp::new_file ().unwrap ();
+        {
+            let mut f = File::create (temp_file.to_path_buf ()).unwrap ();
+            copy (&mut zipitem, &mut f).unwrap ();
+        }
+
+        let f = File::open (temp_file.to_path_buf ()).unwrap ();
+        Ok(f.into_raw_fd ())
+    }
+
     pub fn get_readable_fd<'a> (&'a mut self, name: &'a Path, format: FileFormat, _: bool) -> Option<RawFd> {
         /* We currently don't care about must_exist. */
 
@@ -61,39 +77,38 @@ impl<R: Read + Seek> FinderState<R> {
 
         let mut ext = PathBuf::from (name);
         ext.set_extension (format_to_extension (format));
-        if let Ok(f) = File::open (ext) {
+        if let Ok(f) = File::open (ext.clone ()) {
             return Some(f.into_raw_fd());
         }
 
-        /* OK, let's see if it's in the bundle. */
+        /* OK, let's see if it's in the bundle. If so, we need to extract the
+         * contents to a temporary file that we then unlink, because: (1) the
+         * format file is read in as a gzip file, and the way that it is
+         * created requires that the file be associated with a Unix file
+         * handle. But (2) the file must be seekable, so we can't just use
+         * pipes. The temp file is unlinked at the end of this function, but
+         * the open file handle keeps it around for as long as the progam
+         * needs it. Yay Unix!
+         *
+         * We need to use the zip_to_temp_fd helper because the first ZipResult
+         * we look at keeps a mutable borrow on the ZipArchive.
+         */
 
-        let mut zipitem = match self.zip.by_name (name.to_str ().unwrap ()) {
+        if let Ok(fd) = self.zip_to_temp_fd (name) {
+            return Some(fd);
+        }
+
+        return match self.zip_to_temp_fd (&ext) {
             Err(e) => {
                 if let ZipError::FileNotFound = e {
                     println!("PKGW: failed to locate: {:?}", name);
-                    return None;
+                    None
+                } else {
+                    panic!("error reading bundle: {}", e)
                 }
-                panic!("error reading bundle: {}", e);
             },
-            Ok(f) => f
+            Ok(fd) => Some(fd)
         };
-
-        /* It is. We extract the contents to a temporary file that we then
-         * unlink. We do this because: (1) the format file is read in as a
-         * gzip file, and the way that it is created requires that the file be
-         * associated with a Unix file handle. But (2) the file must be
-         * seekable, so we can't just use pipes. The temp file is unlinked at
-         * the end of this function, but the open file handle keeps it around
-         * for as long as the progam needs it. Yay Unix! */
-
-        let temp_file = Temp::new_file ().unwrap ();
-        {
-            let mut f = File::create (temp_file.to_path_buf ()).unwrap ();
-            copy (&mut zipitem, &mut f).unwrap ();
-        }
-
-        let f = File::open (temp_file.to_path_buf ()).unwrap ();
-        Some(f.into_raw_fd ())
     }
 }
 
