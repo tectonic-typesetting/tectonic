3186fa41edfcdb79d3dcb1dc11b8d0f33e138a62
Peter Williams <peter@newton.cx>
2016-11-21 00:49:33 -0500

    tests/etrip: package this up too

---
+++ tests/etrip/run.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Copyright 2016 The Tectonic Project
+# Licensed under the MIT License.
+
+cd $(dirname $0)
+
+# Throw away stderr since it has a bunch of diagnostic output from our I/O backend.
+../../BUILD/xetex -fmt=etrip -no-pdf -output-comment=etrip etrip >etrip.fot 2>/dev/null
+anybad=false
+
+# Remove first line of log that contains a datestamp
+mv etrip.log etrip.log.tmp
+sed -e 1d etrip.log.tmp >etrip.log
+rm -f etrip.log.tmp
+
+for f in etrip.log etrip.fot etrip.xdv etrip.out ; do
+    if ! cmp $f reference-$f ; then
+	echo >&2 "etrip failed: file $f differs"
+	anybad=true
+    fi
+done
+
+$anybad && exit 1
+
+rm -f etrip.fot etrip.log etrip.xdv etrip.out
