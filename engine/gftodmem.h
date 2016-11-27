0397b4d4fb71cf85ce05f4d3c32d1f4bc5b886a5
Peter Williams <peter@newton.cx>
2016-11-27 16:40:52 -0500

    engine/gftodmem.h: unused; remove

--- engine/gftodmem.h
+++
@@ -1,9 +0,0 @@
-/* This file is included from gftodvi.ch, since variant records are
-   too hard to translate automatically.  */
-
-typedef union {
-   scaled sc;
-   fourquarters qqqq;
-} memoryword;
-
-typedef memoryword memory_word;
