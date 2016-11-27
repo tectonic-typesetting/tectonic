ff01d5a281318afac9766eea03b1ccb6ea3d30b8
Peter Williams <peter@newton.cx>
2016-11-27 17:33:47 -0500

    engine/texmfmp.c: apply remainder of the big #ifs

--- engine/texmfmp.c
+++ engine/texmfmp.c
@@ -980,9 +980,6 @@ make_src_special (strnumber srcfilename, int lineno)
   return (oldpool_ptr);
 }
 
-/* pdfTeX routines also used for e-pTeX, e-upTeX, and XeTeX */
-#if defined (pdfTeX) || defined (epTeX) || defined (eupTeX) || defined(XeTeX)
-
 #include <sys/stat.h>
 #include "md5.h"
 
@@ -995,7 +992,6 @@ make_src_special (strnumber srcfilename, int lineno)
 #  define xfree(p)            do { if (p != NULL) free(p); p = NULL; } while (0)
 #  define MAX_CSTRING_LEN     1024 * 1024
 
-#if !defined (pdfTeX)
 #  define PRINTF_BUF_SIZE     1024
 static char print_buf[PRINTF_BUF_SIZE];
 
@@ -1028,267 +1024,6 @@ void pdftex_fail(const char *fmt, ...)
     print_ln();
     exit(EXIT_FAILURE);
 }
-#endif /* not pdfTeX */
-
-#if !defined(XeTeX)
-
-#define TIME_STR_SIZE 30
-char start_time_str[TIME_STR_SIZE];
-static char time_str[TIME_STR_SIZE];
-    /* minimum size for time_str is 24: "D:YYYYmmddHHMMSS+HH'MM'" */
-
-static void makepdftime(time_t t, char *time_str, boolean utc)
-{
-
-    struct tm lt, gmt;
-    size_t size;
-    int i, off, off_hours, off_mins;
-
-    /* get the time */
-    if (utc) {
-        lt = *gmtime(&t);
-    }
-    else {
-        lt = *localtime(&t);
-    }
-    size = strftime(time_str, TIME_STR_SIZE, "D:%Y%m%d%H%M%S", &lt);
-    /* expected format: "YYYYmmddHHMMSS" */
-    if (size == 0) {
-        /* unexpected, contents of time_str is undefined */
-        time_str[0] = '\0';
-        return;
-    }
-
-    /* correction for seconds: %S can be in range 00..61,
-       the PDF reference expects 00..59,
-       therefore we map "60" and "61" to "59" */
-    if (time_str[14] == '6') {
-        time_str[14] = '5';
-        time_str[15] = '9';
-        time_str[16] = '\0';    /* for safety */
-    }
-
-    /* get the time zone offset */
-    gmt = *gmtime(&t);
-
-    /* this calculation method was found in exim's tod.c */
-    off = 60 * (lt.tm_hour - gmt.tm_hour) + lt.tm_min - gmt.tm_min;
-    if (lt.tm_year != gmt.tm_year) {
-        off += (lt.tm_year > gmt.tm_year) ? 1440 : -1440;
-    } else if (lt.tm_yday != gmt.tm_yday) {
-        off += (lt.tm_yday > gmt.tm_yday) ? 1440 : -1440;
-    }
-
-    if (off == 0) {
-        time_str[size++] = 'Z';
-        time_str[size] = 0;
-    } else {
-        off_hours = off / 60;
-        off_mins = abs(off - off_hours * 60);
-        i = snprintf(&time_str[size], 9, "%+03d'%02d'", off_hours, off_mins);
-        check_nprintf(i, 9);
-    }
-}
-
-void initstarttime(void)
-{
-    if (!start_time_set) {
-        init_start_time ();
-        if (getenv ("SOURCE_DATE_EPOCH")) {
-            makepdftime (start_time, start_time_str, /* utc= */true);
-        } else {
-            makepdftime (start_time, start_time_str,  /* utc= */false);
-        }
-    }
-}
-
-char *makecstring(integer s)
-{
-    static char *cstrbuf = NULL;
-    char *p;
-    static int allocsize;
-    int allocgrow, i, l = str_start[s + 1] - str_start[s];
-    check_buf(l + 1, MAX_CSTRING_LEN);
-    if (cstrbuf == NULL) {
-        allocsize = l + 1;
-        cstrbuf = xmalloc_array(char, allocsize);
-    } else if (l + 1 > allocsize) {
-        allocgrow = allocsize * 0.2;
-        if (l + 1 - allocgrow > allocsize)
-            allocsize = l + 1;
-        else if (allocsize < MAX_CSTRING_LEN - allocgrow)
-            allocsize += allocgrow;
-        else
-            allocsize = MAX_CSTRING_LEN;
-        cstrbuf = xrealloc_array(cstrbuf, char, allocsize);
-    }
-    p = cstrbuf;
-    for (i = 0; i < l; i++)
-        *p++ = str_pool[i + str_start[s]];
-    *p = 0;
-    return cstrbuf;
-}
-
-/* makecfilename
-  input/ouput same as makecstring:
-    input: string number
-    output: C string with quotes removed.
-    That means, file names that are legal on some operation systems
-    cannot any more be used since pdfTeX version 1.30.4.
-*/
-char *makecfilename(integer s)
-{
-    char *name = makecstring(s);
-    char *p = name;
-    char *q = name;
-
-    while (*p) {
-        if (*p != '"')
-            *q++ = *p;
-        p++;
-    }
-    *q = '\0';
-    return name;
-}
-
-void getcreationdate(void)
-{
-    size_t len;
-    initstarttime();
-    /* put creation date on top of string pool and update pool_ptr */
-    len = strlen(start_time_str);
-
-    /* In e-pTeX, "init len => call initstarttime()" (as pdftexdir/utils.c)
-       yields  unintentional output. */
-
-    if ((unsigned) (pool_ptr + len) >= (unsigned) (pool_size)) {
-        pool_ptr = pool_size;
-        /* error by str_toks that calls str_room(1) */
-        return;
-    }
-
-    memcpy(&str_pool[pool_ptr], start_time_str, len);
-    pool_ptr += len;
-}
-
-void getfilemoddate(integer s)
-{
-    struct stat file_data;
-
-    char *file_name = kpse_find_file(makecfilename(s), kpse_tex_format, true);
-    if (file_name == NULL) {
-        return;                 /* empty string */
-    }
-
-    /*recorder_record_input(file_name);*/
-    /* get file status */
-    if (stat(file_name, &file_data) == 0) {
-        size_t len;
-
-        makepdftime(file_data.st_mtime, time_str, /* utc= */false);
-        len = strlen(time_str);
-        if ((unsigned) (pool_ptr + len) >= (unsigned) (pool_size)) {
-            pool_ptr = pool_size;
-            /* error by str_toks that calls str_room(1) */
-        } else {
-            memcpy(&str_pool[pool_ptr], time_str, len);
-            pool_ptr += len;
-        }
-    }
-    /* else { errno contains error code } */
-
-    xfree(file_name);
-}
-
-void getfilesize(integer s)
-{
-    struct stat file_data;
-    int i;
-
-    char *file_name = kpse_find_file(makecfilename(s), kpse_tex_format, true);
-    if (file_name == NULL) {
-        return;                 /* empty string */
-    }
-
-    /*recorder_record_input(file_name);*/
-    /* get file status */
-    if (stat(file_name, &file_data) == 0) {
-        size_t len;
-        char buf[20];
-
-        /* st_size has type off_t */
-        i = snprintf(buf, sizeof(buf),
-                     "%lu", (long unsigned int) file_data.st_size);
-        check_nprintf(i, sizeof(buf));
-        len = strlen(buf);
-        if ((unsigned) (pool_ptr + len) >= (unsigned) (pool_size)) {
-            pool_ptr = pool_size;
-            /* error by str_toks that calls str_room(1) */
-        } else {
-            memcpy(&str_pool[pool_ptr], buf, len);
-            pool_ptr += len;
-        }
-    }
-    /* else { errno contains error code } */
-
-    xfree(file_name);
-}
-
-void getfiledump(integer s, int offset, int length)
-{
-    FILE *f;
-    int read, i;
-    pool_pointer data_ptr;
-    pool_pointer data_end;
-    char *file_name;
-
-    if (length == 0) {
-        /* empty result string */
-        return;
-    }
-
-    if (pool_ptr + 2 * length + 1 >= pool_size) {
-        /* no place for result */
-        pool_ptr = pool_size;
-        /* error by str_toks that calls str_room(1) */
-        return;
-    }
-
-    file_name = kpse_find_file(makecfilename(s), kpse_tex_format, true);
-    if (file_name == NULL) {
-        return;                 /* empty string */
-    }
-
-    /* read file data */
-    f = fopen(file_name, FOPEN_RBIN_MODE);
-    if (f == NULL) {
-        xfree(file_name);
-        return;
-    }
-    /*recorder_record_input(file_name);*/
-    if (fseek(f, offset, SEEK_SET) != 0) {
-        xfree(file_name);
-        return;
-    }
-    /* there is enough space in the string pool, the read
-       data are put in the upper half of the result, thus
-       the conversion to hex can be done without overwriting
-       unconverted bytes. */
-    data_ptr = pool_ptr + length;
-    read = fread(&str_pool[data_ptr], sizeof(char), length, f);
-    fclose(f);
-
-    /* convert to hex */
-    data_end = data_ptr + read;
-    for (; data_ptr < data_end; data_ptr++) {
-        i = snprintf((char *) &str_pool[pool_ptr], 3,
-                     "%.2X", (unsigned int) str_pool[data_ptr]);
-        check_nprintf(i, 3);
-        pool_ptr += i;
-    }
-    xfree(file_name);
-}
-#endif /* not XeTeX */
 
 /* Converts any given string in into an allowed PDF string which is
  * hexadecimal encoded;
@@ -1318,10 +1053,8 @@ void getmd5sum(strnumber s, boolean file)
     md5_byte_t digest[DIGEST_SIZE];
     char outbuf[2 * DIGEST_SIZE + 1];
     int len = 2 * DIGEST_SIZE;
-#if defined(XeTeX)
     char *xname;
     int i;
-#endif
 
     if (file) {
         char file_buf[FILE_BUF_SIZE];
@@ -1355,17 +1088,11 @@ void getmd5sum(strnumber s, boolean file)
     } else {
         /* s contains the data */
         md5_init(&state);
-#if defined(XeTeX)
         xname = gettexstring (s);
         md5_append(&state,
                    (md5_byte_t *) xname,
                    strlen (xname));
         xfree (xname);
-#else
-        md5_append(&state,
-                   (md5_byte_t *) &str_pool[str_start[s]],
-                   str_start[s + 1] - str_start[s]);
-#endif
         md5_finish(&state, digest);
     }
 
@@ -1373,14 +1100,8 @@ void getmd5sum(strnumber s, boolean file)
         /* error by str_toks that calls str_room(1) */
         return;
     }
+
     convertStringToHexString((char *) digest, outbuf, DIGEST_SIZE);
-#if defined(XeTeX)
     for (i = 0; i < 2 * DIGEST_SIZE; i++)
         str_pool[pool_ptr++] = (uint16_t)outbuf[i];
-#else
-    memcpy(&str_pool[pool_ptr], outbuf, len);
-    pool_ptr += len;
-#endif
 }
-
-#endif /* pdfTeX or e-pTeX or e-upTeX or XeTeX */
