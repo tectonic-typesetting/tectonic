dccebe0f97bb2e2ed264dcf8edbe301cfec340aa
Peter Williams <peter@newton.cx>
2016-11-27 19:35:04 -0500

    engine/tidy_kpathutil.c: fold zround.c into here

--- engine/tidy_kpathutil.c
+++ engine/tidy_kpathutil.c
@@ -16,7 +16,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this library; if not, see <http://www.gnu.org/licenses/>.  */
 
-#include <tidy_kpathutil.h>
+#include "w2c-config.h"
 
 /* c-pathch.h */
 
@@ -583,4 +583,43 @@ usagehelp (const_string *message, const_string bug_email)
     }
     printf("\nEmail bug reports to %s.\n", bug_email);
     exit(0);
+}
+
+/* zround.c */
+
+integer
+zround (double r)
+{
+  integer i;
+
+  /* R can be outside the range of an integer if glue is stretching or
+     shrinking a lot.  We can't do any better than returning the largest
+     or smallest integer possible in that case.  It doesn't seem to make
+     any practical difference.  Here is a sample input file which
+     demonstrates the problem, from phil@cs.arizona.edu:
+     	\documentstyle{article}
+	\begin{document}
+	\begin{flushleft}
+	$\hbox{} $\hfill 
+	\filbreak
+	\eject
+    
+     djb@silverton.berkeley.edu points out we should testing against
+     TeX's largest or smallest integer (32 bits), not the machine's.  So
+     we might as well use a floating-point constant, and avoid potential
+     compiler bugs (also noted by djb, on BSDI).  */
+  if (r > 2147483647.0)
+    i = 2147483647;
+  /* should be ...8, but atof bugs are too common */
+  else if (r < -2147483647.0)
+    i = -2147483647;
+  /* Admittedly some compilers don't follow the ANSI rules of casting
+     meaning truncating toward zero; but it doesn't matter enough to do
+     anything more complicated here.  */
+  else if (r >= 0.0)
+    i = (integer)(r + 0.5);
+  else
+    i = (integer)(r - 0.5);
+
+  return i;
 }
