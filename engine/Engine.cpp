f2195ab04b4380187bbd190785a9b352e4f9b373
Peter Williams <peter@newton.cx>
2016-11-27 19:56:07 -0500

    engine/Engine.cpp: fold in teckit-config.h

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -25,7 +25,20 @@ Description:
 
 //#define TRACING	1
 
-#include "teckit-config.h"
+#define HAVE_INTTYPES_H 1
+#define HAVE_IOSTREAM 1
+#define HAVE_LIBZ 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_ZLIB_H 1
+#define STDC_HEADERS 1
+#define VERSION "2.5.4"
 
 #if	(defined(__dest_os) && (__dest_os == __win32_os)) || defined(WIN32)	/* Windows target: little-endian */
 #	undef WORDS_BIGENDIAN
