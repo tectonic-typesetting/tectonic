fb41113f91e4b351a7e15d57d633c5b62056c7ea
Peter Williams <peter@newton.cx>
2016-12-31 12:09:03 -0500

    tests/trip.rs: merge the eTRIP test in here
    This exposes the problem that the Rust test framework wants to run everything
    in simultaneous threads, which is really bad news for us. This can be worked
    around with the RUST_TEST_THREADS environment variable or the --test-threads
    option, but I want everything to Just Work automatically. So, I add a fun
    global static mutex to have the engines not step on each other's toes.

--- tests/trip.rs
+++ tests/trip.rs
@@ -6,7 +6,15 @@
 // of the engine, so you can't do anything to improve the (incredibly poor) UX
 // of the TeX engine without having to fudge what "the TRIP test" is. That is
 // what we have done.
+//
+// Cargo tries to run tests in multiple simultaneous threads, which of course
+// totally fails for Tectonic since the engine has tons of global state. The
+// multithreading can be disabled by setting the RUST_TEST_THREADS environment
+// variable to "1", but that's an annoying solution. So, we use a global mutex
+// to achieve the same effect. Classy.
 
+#[macro_use]
+extern crate lazy_static;
 extern crate tectonic;
 
 use std::collections::HashMap;
@@ -14,6 +22,7 @@ use std::ffi::{OsStr, OsString};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
+use std::sync::Mutex;
 
 use tectonic::io::{IOStack, MemoryIO};
 use tectonic::io::testing::SingleInputFileIO;
@@ -21,6 +30,10 @@ use tectonic::Engine;
 
 const TOP: &'static str = env!("CARGO_MANIFEST_DIR");
 
+
+lazy_static! {
+    static ref LOCK: Mutex<u8> = Mutex::new(0u8);
+}
 
 struct ExpectedInfo {
     name: OsString,
@@ -70,6 +83,8 @@ impl ExpectedInfo {
 
 #[test]
 fn trip_test() {
+    let _guard = LOCK.lock().unwrap(); // until we're thread-safe ...
+
     let mut p = PathBuf::from(TOP);
     p.push("tests");
     p.push("trip");
@@ -115,5 +130,57 @@ fn trip_test() {
     expected_log.test(files);
     expected_xdv.test(files);
     expected_os.test(files);
+    expected_fot.test_data(files.get(OsStr::new("")).unwrap());
+}
+
+
+#[test]
+fn etrip_test() {
+    let _guard = LOCK.lock().unwrap(); // until we're thread-safe ...
+
+    let mut p = PathBuf::from(TOP);
+    p.push("tests");
+    p.push("trip");
+
+    // An IOProvider for the format file.
+    let mut fmt_path = p.clone();
+    fmt_path.push("etrip.fmt");
+    let fmt = SingleInputFileIO::new(&fmt_path);
+
+    // Ditto for the input file.
+    p.push("etrip");
+    p.set_extension("tex");
+    let tex = SingleInputFileIO::new(&p);
+
+    // And the TFM file.
+    p.set_extension("tfm");
+    let tfm = SingleInputFileIO::new(&p);
+
+    // Read in the expected outputs.
+    let expected_log = ExpectedInfo::read(&mut p, "log");
+    let expected_xdv = ExpectedInfo::read(&mut p, "xdv");
+    let expected_fot = ExpectedInfo::read(&mut p, "fot");
+    let expected_out = ExpectedInfo::read(&mut p, "out");
+
+    // MemoryIO layer that will accept the outputs. Save `files` since the
+    // engine consumes `mem`.
+    let mem = MemoryIO::new(true);
+    let files = mem.files.clone();
+
+    // Run the engine!
+    let mut e = Engine::new (IOStack::new(vec![
+        Box::new(mem),
+        Box::new(tex),
+        Box::new(fmt),
+        Box::new(tfm),
+    ]));
+    e.set_output_format ("xdv");
+    e.process("etrip.fmt", "etrip").unwrap();
+
+    // Check that outputs match expectations.
+    let files = &*files.borrow();
+    expected_log.test(files);
+    expected_xdv.test(files);
+    expected_out.test(files);
     expected_fot.test_data(files.get(OsStr::new("")).unwrap());
 }
