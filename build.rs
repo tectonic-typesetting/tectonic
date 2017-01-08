2e7217d9cd0ee6293e652e41d8ed26f3a88414f8
Peter Williams <peter@newton.cx>
2017-01-07 18:49:12 -0600

    build.rs: first-cut attempt for compiling xdvipdfmx into a library

--- build.rs
+++ build.rs
@@ -52,9 +52,10 @@ fn main() {
 
     // Actually I'm not 100% sure that I can't compile the C and C++ code
     // into one library, but who cares?
-    
+
     let mut ccfg = gcc::Config::new();
     let mut cppcfg = gcc::Config::new();
+    let mut dpxcfg = gcc::Config::new();
 
     ccfg.file("tectonic/bmpimage.c")
         .file("tectonic/engine-interface.c")
@@ -86,9 +87,84 @@ fn main() {
         .file("tectonic/XeTeXOTMath.cpp")
         .include(".");
 
+    dpxcfg.file("xdvipdfmx/agl.c")
+        .file("xdvipdfmx/bmpimage.c")
+        .file("xdvipdfmx/cff.c")
+        .file("xdvipdfmx/cff_dict.c")
+        .file("xdvipdfmx/cid.c")
+        .file("xdvipdfmx/cidtype0.c")
+        .file("xdvipdfmx/cidtype2.c")
+        .file("xdvipdfmx/cmap.c")
+        .file("xdvipdfmx/cmap_read.c")
+        .file("xdvipdfmx/cmap_write.c")
+        .file("xdvipdfmx/cs_type2.c")
+        .file("xdvipdfmx/dpxconf.c")
+        .file("xdvipdfmx/dpxcrypt.c")
+        .file("xdvipdfmx/dpxfile.c")
+        .file("xdvipdfmx/dpxutil.c")
+        .file("xdvipdfmx/dvi.c")
+        .file("xdvipdfmx/dvipdfmx.c")
+        .file("xdvipdfmx/epdf.c")
+        .file("xdvipdfmx/error.c")
+        .file("xdvipdfmx/fontmap.c")
+        .file("xdvipdfmx/jp2image.c")
+        .file("xdvipdfmx/jpegimage.c")
+        .file("xdvipdfmx/mem.c")
+        .file("xdvipdfmx/mfileio.c")
+        .file("xdvipdfmx/mpost.c")
+        .file("xdvipdfmx/numbers.c")
+        .file("xdvipdfmx/otl_conf.c")
+        .file("xdvipdfmx/otl_opt.c")
+        .file("xdvipdfmx/pdfcolor.c")
+        .file("xdvipdfmx/pdfdev.c")
+        .file("xdvipdfmx/pdfdoc.c")
+        .file("xdvipdfmx/pdfdraw.c")
+        .file("xdvipdfmx/pdfencoding.c")
+        .file("xdvipdfmx/pdfencrypt.c")
+        .file("xdvipdfmx/pdffont.c")
+        .file("xdvipdfmx/pdfnames.c")
+        .file("xdvipdfmx/pdfobj.c")
+        .file("xdvipdfmx/pdfparse.c")
+        .file("xdvipdfmx/pdfresource.c")
+        .file("xdvipdfmx/pdfximage.c")
+        .file("xdvipdfmx/pkfont.c")
+        .file("xdvipdfmx/pngimage.c")
+        .file("xdvipdfmx/pst.c")
+        .file("xdvipdfmx/pst_obj.c")
+        .file("xdvipdfmx/sfnt.c")
+        .file("xdvipdfmx/spc_color.c")
+        .file("xdvipdfmx/spc_dvipdfmx.c")
+        .file("xdvipdfmx/spc_dvips.c")
+        .file("xdvipdfmx/spc_html.c")
+        .file("xdvipdfmx/spc_misc.c")
+        .file("xdvipdfmx/spc_pdfm.c")
+        .file("xdvipdfmx/spc_tpic.c")
+        .file("xdvipdfmx/spc_util.c")
+        .file("xdvipdfmx/spc_xtx.c")
+        .file("xdvipdfmx/specials.c")
+        .file("xdvipdfmx/subfont.c")
+        .file("xdvipdfmx/t1_char.c")
+        .file("xdvipdfmx/t1_load.c")
+        .file("xdvipdfmx/tfm.c")
+        .file("xdvipdfmx/truetype.c")
+        .file("xdvipdfmx/tt_aux.c")
+        .file("xdvipdfmx/tt_cmap.c")
+        .file("xdvipdfmx/tt_glyf.c")
+        .file("xdvipdfmx/tt_gsub.c")
+        .file("xdvipdfmx/tt_post.c")
+        .file("xdvipdfmx/tt_table.c")
+        .file("xdvipdfmx/type0.c")
+        .file("xdvipdfmx/type1.c")
+        .file("xdvipdfmx/type1c.c")
+        .file("xdvipdfmx/unicode.c")
+        .file("xdvipdfmx/vf.c")
+        .file("xdvipdfmx/xbb.c")
+        .include("xdvipdfmx");
+
     for p in deps.include_paths {
         ccfg.include(&p);
         cppcfg.include(&p);
+        dpxcfg.include(&p);
     }
 
     c_platform_specifics(&mut ccfg);
@@ -96,4 +172,5 @@ fn main() {
 
     ccfg.compile("libtectonic_c.a");
     cppcfg.compile("libtectonic_cpp.a");
+    dpxcfg.compile("libtectonic_dvipdfmx.a");
 }
