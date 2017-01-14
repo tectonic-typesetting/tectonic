101a8a2de9083506007d7c4829cb19778c876fde
Peter Williams <peter@newton.cx>
2017-01-14 17:53:22 -0500

    tectonic/dpx-sfnt.h: standard whitespace futzing

--- tectonic/dpx-sfnt.h
+++ tectonic/dpx-sfnt.h
@@ -1,21 +1,21 @@
 /* This is dvipdfmx, an eXtended version of dvipdfm by Mark A. Wicks.
 
-    Copyright (C) 2002-2016 by Jin-Hwan Cho and Shunsaku Hirata,
-    the dvipdfmx project team.
-    
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+   Copyright (C) 2002-2016 by Jin-Hwan Cho and Shunsaku Hirata,
+   the dvipdfmx project team.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 */
 
 #ifndef _SFNT_H_
@@ -43,26 +43,26 @@ typedef unsigned short uFWord;
 
 struct sfnt_table
 {
-  /* table header */
-  char   tag[4];
-  ULONG  check_sum;
-  ULONG  offset;
-  ULONG  length;
-  char  *data;   /* table data */
+    /* table header */
+    char   tag[4];
+    ULONG  check_sum;
+    ULONG  offset;
+    ULONG  length;
+    char  *data;   /* table data */
 };
 
 #define SFNT_TABLE_REQUIRED (1 << 0)
 
 struct sfnt_table_directory
 {
-  ULONG   version;         /* Fixed for Win */
-  USHORT  num_tables;
-  USHORT  search_range;
-  USHORT  entry_selector;
-  USHORT  range_shift;
-  USHORT  num_kept_tables; /* number of kept tables */
-  char   *flags;           /* keep or omit */
-  struct  sfnt_table *tables;
+    ULONG   version;         /* Fixed for Win */
+    USHORT  num_tables;
+    USHORT  search_range;
+    USHORT  entry_selector;
+    USHORT  range_shift;
+    USHORT  num_kept_tables; /* number of kept tables */
+    char   *flags;           /* keep or omit */
+    struct  sfnt_table *tables;
 };
 
 /* sfnt resource */
@@ -74,15 +74,15 @@ struct sfnt_table_directory
 
 typedef struct
 {
-  int    type;
-  struct sfnt_table_directory *directory;
-  FILE  *stream;
-  ULONG  offset;
+    int    type;
+    struct sfnt_table_directory *directory;
+    FILE  *stream;
+    ULONG  offset;
 } sfnt;
 
 /* Convert sfnt "fixed" type to double */
-#define fixed(a) ((double)((a)%0x10000L)/(double)(0x10000L) + \
- (a)/0x10000L - (((a)/0x10000L > 0x7fffL) ? 0x10000L : 0))
+#define fixed(a) ((double)((a)%0x10000L)/(double)(0x10000L) +           \
+                  (a)/0x10000L - (((a)/0x10000L > 0x7fffL) ? 0x10000L : 0))
 
 /* get_***_*** from numbers.h */
 #define sfnt_get_byte(s)   ((BYTE)   get_unsigned_byte((s)->stream))
@@ -113,7 +113,7 @@ extern ULONG sfnt_find_table_pos       (sfnt *sfont, const char *tag);
 extern ULONG sfnt_locate_table         (sfnt *sfont, const char *tag);
 
 extern void  sfnt_set_table     (sfnt *sfont,
-				 const char *tag, void *data, ULONG length);
+                                 const char *tag, void *data, ULONG length);
 extern int   sfnt_require_table (sfnt *sfont, const char *tag, int must_exist);
 
 extern pdf_obj *sfnt_create_FontFile_stream (sfnt *sfont);
