2e8fe6f28dce8eff01776a56c71e1488cbbfdc39
Peter Williams <peter@newton.cx>
2017-03-05 12:07:33 -0500

    A little bit of API doc work.

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,34 @@
 
 #![recursion_limit = "1024"] // "error_chain can recurse deeply"
 
+//! Tectonic is a complete
+//! [TeX](https://www.tug.org/)/[LaTeX](https://www.latex-project.org/) engine
+//! converted into a standalone library. It is derived from the
+//! [XeTeX](http://xetex.sourceforge.net/) variant of TeX and uses the support
+//! files packages by the [TeX Live](https://www.tug.org/texlive/) project.
+//! Tectonic would not be possible without the hard work that has gone into
+//! these projects.
+//!
+//! Because Tectonic is based on the XeTeX engine, it can take advantage of
+//! the features of modern fonts (TrueType, OpenType, etc.), outputs directly
+//! to the PDF file format, and supports Unicode inputs. Tectonic differs from
+//! other TeX engines in the following ways:
+//!
+//! - Dependencies on environment variables and configuration files have been
+//!   eliminated.
+//! - All I/O is routed through pluggable backends. Support data can be fetched
+//!   from a single “bundle” file, and the engine’s (copious) output can be
+//!   hidden or postprocessed.
+//! - The command-line frontend, `tectonic`, has a modernized user interface that
+//!   never asks for user input.
+//! - The frontend is just a thin shim over the Tectonic Rust crate, so that
+//!   the full engine can be embedded anywhere you can run Rust code.
+//!
+//! Rust API documentation for Tectonic is currently very incomplete. As a
+//! stopgap, please see [the source to the CLI
+//! frontend](https://github.com/pkgw/tectonic/blob/master/src/cli_driver.rs)
+//! for a demonstration of how to run the engine.
+
 extern crate app_dirs;
 extern crate crypto;
 #[macro_use] extern crate error_chain;
