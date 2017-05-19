4b17b3279abc9848b6c01ccd257a1763bbbf9c66
Peter Williams <peter@newton.cx>
2017-05-19 12:26:31 -0400

    Bump the format file version to catch up with all of our changes.

--- src/lib.rs
+++ src/lib.rs
@@ -61,4 +61,4 @@ pub use errors::{Error, ErrorKind, Result};
 
 const APP_INFO: app_dirs::AppInfo = app_dirs::AppInfo {name: "Tectonic", author: "TectonicProject"};
 
-const FORMAT_SERIAL: u32 = 22; // keep synchronized with tectonics/constants.h!!
+const FORMAT_SERIAL: u32 = 23; // keep synchronized with tectonic/constants.h!!
