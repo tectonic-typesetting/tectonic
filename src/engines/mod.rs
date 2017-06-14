tectonic-typesetting/tectonic

commit 7c2e4e622dde666a038154a9c925829e309a988f

engines/mod: Make `format_to_extension` return Vec
* Make `format_to_extension` return a Vector of strings.

* Make use of `set_extension` function of PathBuf instead of creating an
  OsString.

* Add multiple vectorized values as results of `format_to_extension`.

* Fixes #78

Signed-off-by: mr.Shu <mr@shu.io>

src/engines/mod.rs
@@ -132,22 +132,23 @@ impl<'a, I: 'a + IoProvider> ExecutionState<'a, I> {
             return r;
         }
 
-        // Maybe there's a nicer way to alter the extension without turning
-        // `name` into a Path?
+        for e in format_to_extension(format) {
+            let mut ext = PathBuf::from(name);
+            if !ext.set_extension(e) {
+                return OpenResult::NotAvailable
+            }
 
-        let mut ext = PathBuf::from(name);
-        let mut ename = OsString::from(match ext.file_name() {
-            Some(s) => s,
-            None => return OpenResult::NotAvailable
-        });
-        ename.push(format_to_extension(format));
-        ext.set_file_name(ename);
-
-        if let FileFormat::Format = format {
-            self.io.input_open_format(&ext.into_os_string(), self.status)
-        } else {
-            self.io.input_open_name(&ext.into_os_string(), self.status)
+            if let FileFormat::Format = format {
+                if let r @ OpenResult::Ok(_) = self.io.input_open_format(&ext.into_os_string(), self.status) {
+                    return r
+                }
+            } else {
+                if let r @ OpenResult::Ok(_) = self.io.input_open_name(&ext.into_os_string(), self.status) {
+                    return r
+                }
+            }
         }
+        OpenResult::NotAvailable
     }
 
     fn input_open_name_format_gz(&mut self, name: &OsStr, format: FileFormat,
@@ -703,29 +704,29 @@ enum FileFormat {
     Vf,
 }
 
-fn format_to_extension (format: FileFormat) -> &'static str {
+fn format_to_extension (format: FileFormat) -> Vec<&'static str> {
     match format {
-        FileFormat::AFM => ".afm",
-        FileFormat::Bib => ".bib",
-        FileFormat::Bst => ".bst",
-        FileFormat::Cmap => ".cmap", /* XXX: kpathsea doesn't define any suffixes for this */
-        FileFormat::Enc => ".enc",
-        FileFormat::Format => ".fmt.gz",
-        FileFormat::FontMap => ".map",
-        FileFormat::MiscFonts => ".miscfonts", /* XXX: no kpathsea suffixes */
-        FileFormat::Ofm => ".ofm", /* XXX: also .tfm */
-        FileFormat::OpenType => ".otf", /* XXX: also OTF */
-        FileFormat::Ovf => ".ovf", /* XXX: also .vf */
-        FileFormat::Pict => ".pdf", /* XXX: also .eps, .epsi, ... */
-        FileFormat::Pk => ".pk",
-        FileFormat::ProgramData => ".programdata", /* XXX no suffixes */
-        FileFormat::Sfd => ".sfd",
-        FileFormat::Tex => ".tex", /* also .{sty,cls,fd,aux,bbl,def,clo,ldf} */
-        FileFormat::TexPsHeader => ".pro",
-        FileFormat::TFM => ".tfm",
-        FileFormat::TrueType => ".ttf", /* XXX: also .ttc, .TTF, .TTC, .dfont */
-        FileFormat::Type1 => ".pfa", /* XXX: also .pfb */
-        FileFormat::Vf => ".vf",
+        FileFormat::AFM => vec!["afm"],
+        FileFormat::Bib => vec!["bib"],
+        FileFormat::Bst => vec!["bst"],
+        FileFormat::Cmap => vec!["cmap"], /* XXX: kpathsea doesn't define any suffixes for this */
+        FileFormat::Enc => vec!["enc"],
+        FileFormat::Format => vec!["fmt.gz"],
+        FileFormat::FontMap => vec!["map"],
+        FileFormat::MiscFonts => vec!["miscfonts"], /* XXX: no kpathsea suffixes */
+        FileFormat::Ofm => vec!["ofm"], /* XXX: also .tfm */
+        FileFormat::OpenType => vec!["otf", "OTF"],
+        FileFormat::Ovf => vec!["ovf", "vf"],
+        FileFormat::Pict => vec!["pdf", "jpg", "eps", "epsi"], /* XXX: also .eps, .epsi, ... */
+        FileFormat::Pk => vec!["pk"],
+        FileFormat::ProgramData => vec!["programdata"], /* XXX no suffixes */
+        FileFormat::Sfd => vec!["sfd"],
+        FileFormat::Tex => vec!["tex", "sty", "cls", "fd", "aux", "bbl", "def", "clo", "ldf"],
+        FileFormat::TexPsHeader => vec!["pro"],
+        FileFormat::TFM => vec!["tfm"],
+        FileFormat::TrueType => vec!["ttf", "ttc", "TTF", "TTC", "dfont"],
+        FileFormat::Type1 => vec!["pfa", "pfb"],
+        FileFormat::Vf => vec!["vf"],
     }
 }
 

commit b8de2c24d62f3668e3e494696491ce34ceb7b93d

Do not change the extension of files requested by the engine.
Fixes a regression caused by `7c2e4e622dde666a038154a9c925829e309a988f`,
for instance for `tests/xenia/paper.tex`.

src/engines/mod.rs
@@ -17,7 +17,7 @@ use libc;
 use std::ffi::{CStr, OsStr, OsString};
 use std::io::{Read, SeekFrom, Write};
 use std::os::unix::ffi::OsStrExt;
-use std::path::PathBuf;
+use std::path::Path;
 use std::{io, ptr, slice};
 
 use digest::DigestData;
@@ -121,32 +121,38 @@ impl<'a, I: 'a + IoProvider> ExecutionState<'a, I> {
     // Helpers.
 
     fn input_open_name_format(&mut self, name: &OsStr, format: FileFormat) -> OpenResult<InputHandle> {
-        // TODO: for some formats we should check multiple extensions, not
-        // just one.
-
         let r = if let FileFormat::Format = format {
             self.io.input_open_format(name, self.status)
         } else {
             self.io.input_open_name(name, self.status)
         };
 
-        if let OpenResult::NotAvailable = r {
-        } else {
-            return r;
+        let path = Path::new(name);
+
+        match r {
+            OpenResult::NotAvailable if path.extension().is_some() =>
+                // Do not change the extension if provided.
+                return OpenResult::NotAvailable,
+            OpenResult::NotAvailable => {},
+            r => return r,
         }
 
+        if path.file_name().is_none() {
+            // No file name, Path::set_extension will not do anything, so why even try?
+            return OpenResult::NotAvailable;
+        }
+
+        let mut ext = path.to_owned();
+
         for e in format_to_extension(format) {
-            let mut ext = PathBuf::from(name);
-            if !ext.set_extension(e) {
-                return OpenResult::NotAvailable
-            }
+            ext.set_extension(e);
 
             if let FileFormat::Format = format {
-                if let r @ OpenResult::Ok(_) = self.io.input_open_format(&ext.into_os_string(), self.status) {
+                if let r @ OpenResult::Ok(_) = self.io.input_open_format(ext.as_ref(), self.status) {
                     return r
                 }
             } else {
-                if let r @ OpenResult::Ok(_) = self.io.input_open_name(&ext.into_os_string(), self.status) {
+                if let r @ OpenResult::Ok(_) = self.io.input_open_name(ext.as_ref(), self.status) {
                     return r
                 }
             }