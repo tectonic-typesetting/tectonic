9acbfc1f8bddd37326d62138fdaf888b40553b02
Ronny Chevalier <chevalier.ronny@gmail.com>
2017-06-20 22:32:45 +0200

    tectonic: remove duplicated xtoi function
    It keeps the function in dpx-util.c, but it uses the implementation
    from dpx-pdfparse since it was clearer.

--- tectonic/dpx-dpxutil.c
+++ tectonic/dpx-dpxutil.c
@@ -36,13 +36,13 @@ int
 xtoi (char c)
 {
   if (c >= '0' && c <= '9')
-    return (c - '0');
-  else if (c >= 'a' && c <= 'f')
-    return (c - 'W');
-  else if (c >= 'A' && c <= 'F')
-    return (c - '7');
-  else
-    return -1;
+    return c - '0';
+  if (c >= 'A' && c <= 'F')
+    return (c - 'A') + 10;
+  if (c >= 'a' && c <= 'f')
+    return (c - 'a') + 10;
+
+  return -1;
 }
 
 void
--- tectonic/dpx-pdfparse.c
+++ tectonic/dpx-pdfparse.c
@@ -39,6 +39,8 @@
 
 #include <tectonic/dpx-pdfparse.h>
 
+#include <tectonic/dpx-dpxutil.h>
+
 /* PDF */
 #ifdef  is_space
 #undef  is_space
@@ -63,8 +65,6 @@ static struct {
 } parser_state = {
   0
 };
-
-static int xtoi (char ch);
 
 static const char *save = NULL;
 
@@ -562,19 +562,6 @@ parse_pdf_literal_string (const char **pp, const char *endptr)
 /*
  * PDF Hex String
  */
-static int
-xtoi (char ch)
-{
-  if (ch >= '0' && ch <= '9')
-    return ch - '0';
-  if (ch >= 'A' && ch <= 'F')
-    return (ch - 'A') + 10;
-  if (ch >= 'a' && ch <= 'f')
-    return (ch - 'a') + 10;
-
-  return -1;
-}
-
 static pdf_obj *
 parse_pdf_hex_string (const char **pp, const char *endptr)
 {
