f1f33cb161c28fb3318dd56ee3cd8747ff4337da
Peter Williams <peter@newton.cx>
2017-11-15 20:50:29 -0500

    Version 0.1.7.

--- src/cli_driver.rs
+++ src/cli_driver.rs
@@ -991,7 +991,7 @@ fn inner(matches: ArgMatches, config: PersistentConfig, status: &mut TermcolorSt
 
 fn main() {
     let matches = App::new("Tectonic")
-        .version("0.1.7-dev")
+        .version("0.1.7")
         .about("Process a (La)TeX document.")
         .arg(Arg::with_name("format")
              .long("format")
