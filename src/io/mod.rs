1eef8ade2446ccd7d6e6c6c736a33202ad66a5be
Peter Williams <peter@newton.cx>
2018-01-14 10:57:08 -0500

    src/io/mod.rs: add convenience OpenResult::must_exist()

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -283,6 +283,17 @@ impl<T> OpenResult<T> {
             false
         }
     }
+
+    /// Convert this object into a plain Result, erroring if the item was not available.
+    pub fn must_exist(self) -> Result<T> {
+        use std::io;
+
+        match self {
+            OpenResult::Ok(t) => Ok(t),
+            OpenResult::Err(e) => Err(e),
+            OpenResult::NotAvailable => Err(io::Error::new(io::ErrorKind::NotFound, "not found").into()),
+        }
+    }
 }
 
 
