tectonic-typesetting/tectonic

commit bcdc052aafc59eefaa5032bf4e4bd1e14509d7c4

Replace only usage of concat() with purpose-specific code.

tectonic/xetexini.c
@@ -3772,8 +3772,14 @@ tt_misc_initialize(char *dump_name)
     /* Miscellaneous initializations that were originally done in the main()
      * driver routines. */
 
-    TEX_format_default = concat (" ", dump_name); /* for Pascal style */
-    format_default_length = strlen (TEX_format_default + 1);
+    /* TEX_format_default must get a leading space character for Pascal
+     * style string magic. */
+
+    size_t len = strlen (dump_name);
+    TEX_format_default = xmalloc (len + 2);
+    TEX_format_default[0] = ' ';
+    strcpy (TEX_format_default + 1, dump_name);
+    format_default_length = len + 2;
 
     /* Signal handling done with global variables. C'est la vie. */
 

tectonic/internals.h
@@ -123,7 +123,6 @@ extern int pdf_get_rect(char* filename, int page_num, int pdf_box, real_rect* bo
 extern int pdf_count_pages(char* filename);
 
 /* tidy_kpathutil.c */
-extern string concat (const_string s1, const_string s2);
 extern string concat3 (const_string, const_string, const_string);
 extern string xstrdup (const_string s);
 extern const_string xbasename (const_string name);

tectonic/tidy_kpathutil.c
@@ -54,23 +54,6 @@ concat3 (const_string s1,  const_string s2,  const_string s3)
   return answer;
 }
 
-/* concat.c */
-
-/* Return the concatenation of S1 and S2.  See `concatn.c' for a
-   `concatn', which takes a variable number of arguments.  */
-
-string
-concat (const_string s1,  const_string s2)
-{
-  unsigned s1len = strlen(s1);
-  unsigned s2len = strlen(s2);
-  string answer = (string) xmalloc (s1len + s2len + 1);
-  strcpy (answer, s1);
-  strcat (answer + s1len, s2);
-
-  return answer;
-}
-
 
 /* line.c */
 

commit 2d4da5c7250a2346d4e6b55d116fa090aaa6224f

fix off-by-one (uninitialized read)
looks to be introduced by bcdc052a.

tectonic/xetexini.c
@@ -3996,7 +3996,7 @@ tt_run_engine(char *dump_name, char *input_file_name)
     TEX_format_default = xmalloc (len + 2);
     TEX_format_default[0] = ' ';
     strcpy (TEX_format_default + 1, dump_name);
-    format_default_length = len + 2;
+    format_default_length = len + 1;
 
     /* Not sure why these get custom initializations. */
 