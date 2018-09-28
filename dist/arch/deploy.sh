f613f456f0b44c8f212bf68596c1eed24c7c358d
Peter Williams <peter@newton.cx>
2018-09-28 19:41:42 -0400

    dist/arch: iterate more

--- dist/arch/deploy.sh
+++ dist/arch/deploy.sh
@@ -6,9 +6,10 @@ cd "$TRAVIS_BUILD_DIR/dist/arch"
 # Set up to run makepkg
 wget https://www.archlinux.org/packages/core/x86_64/pacman/download/ -O pacman.pkg.tar.xz
 tar -Jxf pacman.pkg.tar.xz
-export PATH="$PATH:$(pwd)/usr/bin"
+bindir="$(pwd)/usr/bin"
+export PATH="$bindir:$PATH"
 export LIBRARY="$(pwd)/usr/share/makepkg"
-CONFIG="$(pwd)/etc/makepkg.conf"
+config="$(pwd)/etc/makepkg.conf"
 
 # Get the repo
 git config --global --add core.sshCommand "ssh -o StrictHostKeyChecking=false"
@@ -17,7 +18,7 @@ git clone ssh://aur@aur.archlinux.org/tectonic.git aur
 # Update it
 cp PKGBUILD aur
 cd aur
-makepkg --config="$CONFIG" --printsrcinfo >.SRCINFO
+/bin/bash "$bindir/makepkg" --config="$config" --printsrcinfo >.SRCINFO
 
 # Commit
 git add PKGBUILD .SRCINFO
