5e45045629155444dc3b36aecb47afdb0608c864
Peter Williams <peter@newton.cx>
2018-09-28 19:11:47 -0400

    dist/arch: iterate on how this works
    We're actually running the script now, at least. I think I understand why the
    previous iteration didn't work (`pkgbuild-introspection` was removed and it
    hung asking for an SSH host key confirmation), so hopefully this will work ...
    
    Also, it turns out I need to update the PKGBUILD file every release!

--- dist/arch/deploy.sh
+++ dist/arch/deploy.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-set -x
+set -ex
 cd "$TRAVIS_BUILD_DIR/dist/arch"
 
-# Obtain mksrcinfo
-wget https://www.archlinux.org/packages/community/any/pkgbuild-introspection/download/ -O pkgbuild-introspection.tar.xz
-tar -Jxf pkgbuild-introspection.tar.xz usr/bin/mksrcinfo
-PATH="$PATH:$(pwd)/usr/bin"
+# Set up to run makepkg
+wget https://www.archlinux.org/packages/core/x86_64/pacman/download/ -O pacman.pkg.tar.xz
+tar -Jxf pacman.pkg.tar.xz
+export PATH="$PATH:$(pwd)/usr/bin"
+export LIBRARY="$(pwd)/usr/share/makepkg"
+CONFIG="$(pwd)/etc/makepkg.conf"
 
-# Prepare directory for deploy
+# Get the repo
+git config --global --add core.sshCommand "ssh -o StrictHostKeyChecking=false"
 git clone ssh://aur@aur.archlinux.org/tectonic.git aur
+
+# Update it
 cp PKGBUILD aur
 cd aur
-mksrcinfo
+makepkg --config="$CONFIG" --printsrcinfo >.SRCINFO
 
+# Commit
 git add PKGBUILD .SRCINFO
 git config user.email "tectonic-deploy@example.com"
 git config user.name "tectonic-deploy"
