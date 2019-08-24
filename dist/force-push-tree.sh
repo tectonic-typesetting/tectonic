0b61d39f96101e6c629760b309d1b6a3aa74ed0c
Peter Williams <peter@newton.cx>
2019-08-24 11:42:39 -0400

    dist: prototype infrastructure to build and deploy the docs/ mdbook
    Building is straightforward - there is a binary mdbook artifact we can use.
    
    I've thought about how to deploy and I don't see one obvious best solution. I
    can foresee wanting to graft multiple statically-generated HTML trees into the
    overall https://tectonic-typesetting.github.io/ URL structure. Here, the model
    is that we'll use a repo named `book` that contains only static HTML that is
    served at the root https://tectonic-typesetting.github.io/book via GitHub
    pages. When books are deployed, the compiled HTML is copied into that
    structure (possibly in a subdirectory; e.g. `latest/` and `1.0/`),
    amend-committed, and force-pushed up. It's a bit gross but it's the cleanest
    way I can think of to get the HTML grafting through GitHub pages. And of
    course all of the important history is stored in this, the source repository.

---
+++ dist/force-push-tree.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+# Copyright 2019 The Tectonic Project
+# Licensed under the MIT License.
+
+# A helper script to deploy a tree of files to another Git repository by
+# copying them, amend-committing, and force-pushing. The main use case for
+# this script is to deploy generated HTML trees to make them available using
+# GitHub pages.
+#
+# Arguments:
+#
+# $1 - path to the file tree to be deployed
+# $2 - HTTPS URL of the GitHub repository that will receive the files
+# $3 - path within the destination Git repository where the tree will land
+# $4 - brief free text identifying the commit/version of what's being deployed
+#      (used in the Git logs)
+#
+# Environment:
+#
+# $GITHUB_TOKEN - a bearer-token authentication token that can be used to
+#   authenticate writes to the repository specified by $2.
+#
+# See the code for how the token authentication is set up -- it is somewhat
+# magical.
+
+set -e
+set -x # TEMP DEBUG
+
+# Parameters
+
+src_path="$1"
+dest_repo_url="$2"
+dest_repo_path="$3"
+commit_desc="$4"
+
+# Configuration that we expect to be stable.
+
+git_user_email="notifications@github.com"
+git_user_name="Tectonic CI"
+
+# Set up Git and authentication.
+# Derived from: https://www.appveyor.com/docs/how-to/git-push/
+
+echo "Setting up Git ..."
+git config --global credential.helper store
+echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" >~/.git-credentials
+git config --global user.email "$git_user_email"
+git config --global user.name "$git_user_name"
+
+# Set up the target repo.
+
+echo "Cloning target repository $dest_repo_url ..."
+tmprepo="$(mktemp -d)"
+git clone "$dest_repo_url" "$tmprepo"
+mkdir -p "$tmprepo/$dest_repo_path"
+rm -rf "$tmprepo/$dest_repo_path"
+
+# Update the HTML and commit.
+
+echo "Committing and pushing changes ..."
+cp -a "$src_path" "$tmprepo/$dest_repo_path"
+
+pushd "$tmprepo"
+git add "$dest_repo_path"
+git commit --amend -m "Most recent update: $dest_repo_path - $commit_desc"
+git push -f
+popd
+
+# And that's it.
+echo "Success."
--- dist/travis.sh
+++ dist/travis.sh
@@ -218,8 +218,18 @@ if $is_main_build ; then
     travis_fold_end cargo_kcov
 fi
 
-# If we're a "continuous deployment" build, we should push up artifacts for
-# the "continuous" pseudo-release. Right now, all we do is make an AppImage
+# We also build the docs/ mdbook for the main build.
+
+if $is_main_build ; then
+    travis_fold_start docs_mdbook "mdbook build docs" verbose
+    dist/build-mdbook.sh docs
+    travis_fold_end docs_mdbook
+fi
+
+# If we're a "continuous deployment" build, we should push out various artifacts.
+#
+# We maintain a "continuous" pseudo-release on GitHub for binary artifacts
+# that people might want to download. Right now, all we do is make an AppImage
 # for the main build. I believe that the upload script that we use deletes the
 # "continuous" release every time it runs, so if we want different elements of
 # the build matrix to contribute various artifacts, we're going to need to
@@ -241,6 +251,14 @@ if $is_continuous_deployment_build; then
         UPDATE_INFORMATION="gh-releases-zsync|$repo_info|continuous|tectonic-*.AppImage.zsync" \
             dist/appimage/build.sh
         bash ./upload.sh dist/appimage/tectonic-*.AppImage*
+
+        # Deploy the docs book
+        dist/force-push-tree.sh \
+            docs/book \
+            https://github.com/tectonic-typesetting/book.git \
+            latest \
+            "docs mdbook @ $TRAVIS_COMMIT"
+
         travis_fold_end continuous
     fi
 
