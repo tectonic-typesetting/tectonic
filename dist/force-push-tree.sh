f4dcb1c8e455b45d0fb3d757bdb6aafdba6b508a
Peter Williams <peter@newton.cx>
2020-09-06 16:39:35 -0400

    dist: work on wiring up the continuous-deployment release
    Now that Cranko has gained a couple of utility commands, we should
    be able to reproduce this functionality ...

--- dist/force-push-tree.sh
+++ dist/force-push-tree.sh
@@ -15,13 +15,8 @@
 # $4 - brief free text identifying the commit/version of what's being deployed
 #      (used in the Git logs)
 #
-# Environment:
-#
-# $GITHUB_TOKEN - a bearer-token authentication token that can be used to
-#   authenticate writes to the repository specified by $2.
-#
-# See the code for how the token authentication is set up -- it is somewhat
-# magical.
+# We assume that GitHub commit creation and push authentication have been set up
+# externally.
 
 set -e
 
@@ -31,20 +26,6 @@ src_path="$1"
 dest_repo_url="$2"
 dest_repo_path="$3"
 commit_desc="$4"
-
-# Configuration that we expect to be stable.
-
-git_user_email="notifications@github.com"
-git_user_name="Tectonic CI"
-
-# Set up Git and authentication.
-# Derived from: https://www.appveyor.com/docs/how-to/git-push/
-
-echo "Setting up Git ..."
-git config --global credential.helper store
-echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" >~/.git-credentials
-git config --global user.email "$git_user_email"
-git config --global user.name "$git_user_name"
 
 # Set up the target repo.
 
