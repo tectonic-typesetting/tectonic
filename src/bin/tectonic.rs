34a6721acf01315b0bc517086378953be5ba475b
ralismark <timmy.yao@outlook.com.au>
2020-10-10 21:12:28 +1100

    Parse -Z better

--- src/bin/tectonic.rs
+++ src/bin/tectonic.rs
@@ -16,7 +16,7 @@ use tectonic::errors::{ErrorKind, Result};
 use tectonic::status::plain::PlainStatusBackend;
 use tectonic::status::termcolor::TermcolorStatusBackend;
 use tectonic::status::{ChatterLevel, StatusBackend};
-use tectonic::unstable_opts::UnstableOptions;
+use tectonic::unstable_opts::{UnstableArg, UnstableOptions};
 use tectonic::{errmsg, tt_error, tt_note};
 
 #[derive(Debug, StructOpt)]
@@ -74,17 +74,14 @@ struct CliOptions {
     /// The directory in which to place output files [default: the directory containing <input>]
     #[structopt(name = "outdir", short, long, parse(from_os_str))]
     outdir: Option<PathBuf>,
-    /// Unstable options
-    #[structopt(
-        name = "options",
-        short = "Z",
-        number_of_values = 1,
-        empty_values = false
-    )]
-    unstable: Vec<String>,
+    /// Unstable options. Pass -Zhelp to show a list
+    // TODO we can't pass -Zhelp without also passing <input>
+    #[structopt(name = "option", short = "Z", number_of_values = 1)]
+    unstable: Vec<UnstableArg>,
 }
 fn inner(args: CliOptions, config: PersistentConfig, status: &mut dyn StatusBackend) -> Result<()> {
-    let unstable = UnstableOptions::from_strings(args.unstable.iter().map(String::as_str))?;
+    let unstable = UnstableOptions::from_unstable_args(args.unstable.into_iter());
+
     let mut sess_builder = ProcessingSessionBuilder::default();
     let format_path = args.format;
     sess_builder
--- src/unstable_opts.rs
+++ src/unstable_opts.rs
@@ -8,51 +8,78 @@
 //! to be reliable or very polished. In particular, many of these prevent the build from being
 //! reproducible.
 
-use crate::errors::Result;
+use crate::errors::{Error, Result};
 use std::default::Default;
+use std::str::FromStr;
 
+const HELPMSG: &str = r#"Available unstable options:
+
+    -Z help                     Lists all unstable options
+    -Z paper-size=<spec>        Change the default paper size [default: letter]
+    -Z shell-escape             Enable \write18
+"#;
+
+// Each entry of this should correspond to a field of UnstableOptions.
 #[derive(Debug)]
+pub enum UnstableArg {
+    Help,
+    PaperSize(String),
+    ShellEscapeEnabled,
+}
+
+impl FromStr for UnstableArg {
+    type Err = Error;
+
+    /// Parse from the argument to -Z
+    fn from_str(s: &str) -> Result<Self> {
+        let mut splitter = s.splitn(2, '=');
+        let arg = splitter.next().unwrap(); // splitn will always have at least 1 item
+        let value = splitter.next();
+
+        // For structopt/clap, if you pass a value to a flag which doesn't accept one, it's
+        // silently ignored.
+
+        match arg {
+            "help" => Ok(UnstableArg::Help),
+
+            "paper-size" => value
+                .ok_or_else(|| {
+                    "'-Z paper-size <spec>' requires a value but none was supplied".into()
+                })
+                .map(|s| UnstableArg::PaperSize(s.to_string())),
+
+            "shell-escape" => Ok(UnstableArg::ShellEscapeEnabled),
+
+            _ => Err(format!("Unknown unstable option '{}'", arg).into()),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
 pub struct UnstableOptions {
-    /// The output paper size to use, instead of "letter"
     pub paper_size: Option<String>,
-    /// Whether to enable shell escape
     pub shell_escape: bool,
 }
 
-impl Default for UnstableOptions {
-    fn default() -> Self {
-        Self {
-            paper_size: None,
-            shell_escape: false,
-        }
-    }
-}
-
 impl UnstableOptions {
-    pub fn parse_single(&mut self, opt: &str) -> Result<()> {
-        let mut splitted = opt.splitn(2, '=');
-        let name = splitted.next().unwrap();
-        let arg = splitted.next().ok_or("option requres an argument");
-
-        match name {
-            "paper-size" => self.paper_size = Some(arg?.to_string()),
-            "shell-escape" => self.shell_escape = true,
-            "no-shell-escape" => self.shell_escape = false,
-            _ => return Err("invalid option".into()),
-        }
-
-        Ok(())
-    }
-
-    pub fn from_strings<'a, I>(options: I) -> Result<Self>
+    pub fn from_unstable_args<I>(uargs: I) -> Self
     where
-        I: Iterator<Item = &'a str>,
+        I: Iterator<Item = UnstableArg>,
     {
-        let mut config = Self::default();
-        for arg in options {
-            config.parse_single(arg)?;
+        let mut opts = UnstableOptions::default();
+
+        for u in uargs {
+            use UnstableArg::*;
+            match u {
+                Help => {
+                    print!("{}", HELPMSG);
+                    std::process::exit(0);
+                }
+                PaperSize(size) => opts.paper_size = Some(size),
+                ShellEscapeEnabled => opts.shell_escape = true,
+            }
         }
 
-        Ok(config)
+        opts
     }
 }
