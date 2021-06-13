562442d22c68dc826aa0706d643fe5b46eaf84e2
Peter Williams <peter@newton.cx>
2021-06-13 11:54:23 -0400

    src/test_util.rs: use DirBundle here

--- src/test_util.rs
+++ src/test_util.rs
@@ -35,13 +35,13 @@
 //! That call simultaneously tells this module where to find the test assets,
 //! and also activates the test mode.
 
-use std::{collections::HashSet, env, ffi::OsStr, fs, path::PathBuf};
-use tectonic_bundles::Bundle;
+use std::{env, ffi::OsStr, path::PathBuf};
+use tectonic_bundles::{dir::DirBundle, Bundle};
 use tectonic_errors::Result;
 
 use crate::{
     digest::DigestData,
-    io::{FilesystemIo, InputHandle, IoProvider, OpenResult},
+    io::{InputHandle, IoProvider, OpenResult},
     status::StatusBackend,
 };
 
@@ -102,16 +102,15 @@ pub fn test_path(parts: &[&str]) -> PathBuf {
 }
 
 /// Utility for being able to treat the "assets/" directory as a bundle.
-pub struct TestBundle(FilesystemIo);
+///
+/// I think we want to always wrap DirBundle so that we can override
+/// `get_digest()`? But once DirBundle implements `get_digest()` for real we
+/// could consider just dropping this type altogether.
+pub struct TestBundle(DirBundle);
 
 impl Default for TestBundle {
     fn default() -> Self {
-        TestBundle(FilesystemIo::new(
-            &test_path(&["assets"]),
-            false,
-            false,
-            HashSet::new(),
-        ))
+        TestBundle(DirBundle::new(&test_path(&["assets"])))
     }
 }
 
@@ -131,21 +130,7 @@ impl Bundle for TestBundle {
         Ok(DigestData::zeros())
     }
 
-    fn all_files(&mut self, _status: &mut dyn StatusBackend) -> Result<Vec<String>> {
-        // XXX: this is copy/paste of DirBundle.
-        let mut files = Vec::new();
-
-        for entry in fs::read_dir(&self.0.root())? {
-            let entry = entry?;
-
-            // This catches both regular files and symlinks:`
-            if !entry.file_type()?.is_dir() {
-                if let Some(s) = entry.file_name().to_str() {
-                    files.push(s.to_owned());
-                }
-            }
-        }
-
-        Ok(files)
+    fn all_files(&mut self, status: &mut dyn StatusBackend) -> Result<Vec<String>> {
+        self.0.all_files(status)
     }
 }
