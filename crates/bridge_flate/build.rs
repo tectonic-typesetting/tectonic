b0eaef3dbec85b2d50db6c250ce3def2a913cc00
Peter Williams <peter@newton.cx>
2021-06-17 10:07:15 -0400

    Switch to dev-time rather than build-time cbindgen
    Following up on #788, my no-network workaround seemed to work OK, but
    then I ran into problems with `cargo metadata` wanting to create a
    Cargo.lock file in a read-only filesystem. I don't see any way to
    prevent the cbindgen build.rs invocation from triggering this.
    
    But ... we can just use cbindgen as a standalone tool and save the
    generated headers in the repo. I liked the idea of doing things
    on-the-fly in build.rs, but I can't think of any reason that we *have*
    to, and the dev-time approach is faster and simpler, and it turns out
    that it also solves out docs.rs problems. So it's a pretty clear
    decision.
    
    Also, I don't expect these generated headers to vary often. But I
    documented how to regenerate them in their respective crate READMEs.

--- crates/bridge_flate/build.rs
+++ crates/bridge_flate/build.rs
@@ -4,45 +4,14 @@
 use std::{env, path::PathBuf};
 
 fn main() {
-    let outdir = env::var("OUT_DIR").unwrap();
+    let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
+
+    let mut include_dir = manifest_dir;
+    include_dir.push("include");
 
     // Cargo exposes this as the environment variable DEP_XXX_INCLUDE, where XXX
     // is the "links" setting in Cargo.toml. This is the key element that allows
     // us to have a network of crates containing both C/C++ and Rust code that
     // all interlink.
-    println!("cargo:include={}", outdir);
-
-    let mut header_path: PathBuf = outdir.into();
-    header_path.push("tectonic_bridge_flate.h");
-
-    let mut config = cbindgen::Config {
-        cpp_compat: true,
-        ..Default::default()
-    };
-    config.enumeration.prefix_with_name = true;
-
-    let mut manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
-
-    // 2021 June: work around https://github.com/tectonic-typesetting/tectonic/issues/788
-    if env::var_os("DOCS_RS").is_some() {
-        env::set_var("CARGO_NET_OFFLINE", "true");
-    }
-
-    cbindgen::Builder::new()
-        .with_config(config)
-        .with_crate(&manifest_dir)
-        .with_language(cbindgen::Language::C)
-        .with_include_guard("TECTONIC_BRIDGE_FLATE_H")
-        .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file(&header_path);
-
-    // Workaround so that we can `cargo package` this crate. Cf
-    // https://github.com/eqrion/cbindgen/issues/560 . cbindgen calls `cargo
-    // metadata` which creates a new Cargo.lock file when building this crate as
-    // part of its packaging process. This isn't noticed in regular builds since
-    // they occur in a workspace context. Lame but effective solution:
-    // unconditionally blow away the file.
-    manifest_dir.push("Cargo.lock");
-    let _ignored = std::fs::remove_file(&manifest_dir);
+    println!("cargo:include={}", include_dir.display());
 }
