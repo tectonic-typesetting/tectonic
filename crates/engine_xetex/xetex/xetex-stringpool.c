d9cd766999b22344f29aa9e35d49ab83abae0608
Peter Williams <peter@newton.cx>
2021-12-29 17:50:35 -0500

    engine_xetex: tidy up the stringpool functions a bit

--- crates/engine_xetex/xetex/xetex-stringpool.c
+++ crates/engine_xetex/xetex/xetex-stringpool.c
@@ -39,151 +39,168 @@ load_pool_strings(int32_t spare_size)
 }
 
 
-
-int32_t length(str_number s)
+int32_t
+length(str_number s)
 {
-    if ((s >= 65536L))
-        return str_start[(s + 1) - 65536L] - str_start[(s) - 65536L];
-    else if ((s >= 32) && (s < 127))
+    if (s >= 65536L)
+        return str_start[s + 1 - 65536L] - str_start[s - 65536L];
+    if (s >= 32 && s < 127)
         return 1;
-    else if ((s <= 127))
+    if (s <= 127)
         return 3;
-    else if ((s < 256))
+    if (s < 256)
         return 4;
-    else
-        return 8;
+    return 8;
 }
 
-str_number make_string(void)
+
+str_number
+make_string(void)
 {
     if (str_ptr == max_strings)
         overflow("number of strings", max_strings - init_str_ptr);
+
     str_ptr++;
     str_start[str_ptr - TOO_BIG_CHAR] = pool_ptr;
     return str_ptr - 1;
 }
 
-void append_str(str_number s)
+
+void
+append_str(str_number s)
 {
     int32_t i;
     pool_pointer j;
-    i = length(s);
-    {
-        if (pool_ptr + i > pool_size)
-            overflow("pool size", pool_size - init_pool_ptr);
-    }
-    j = str_start[(s) - 65536L];
-    while ((i > 0)) {
 
-        {
-            str_pool[pool_ptr] = str_pool[j];
-            pool_ptr++;
-        }
+    i = length(s);
+
+    if (pool_ptr + i > pool_size)
+        overflow("pool size", pool_size - init_pool_ptr);
+
+    j = str_start[s - 65536L];
+
+    while (i > 0) {
+        str_pool[pool_ptr] = str_pool[j];
+        pool_ptr++;
         j++;
         i--;
     }
 }
 
-bool str_eq_buf(str_number s, int32_t k)
+
+bool
+str_eq_buf(str_number s, int32_t k)
 {
     pool_pointer j;
-    j = str_start[(s) - 65536L];
-    while (j < str_start[(s + 1) - 65536L]) {
 
+    j = str_start[s - 65536L];
+
+    while (j < str_start[s + 1 - 65536L]) {
         if (buffer[k] >= 65536L) {
-
             if (str_pool[j] != 55296L + (buffer[k] - 65536L) / 1024) {
                 return false;
             } else if (str_pool[j + 1] != 56320L + (buffer[k] - 65536L) % 1024) {
                 return false;
-            } else
+            } else {
                 j++;
+            }
         } else if (str_pool[j] != buffer[k]) {
             return false;
         }
+
         j++;
         k++;
     }
+
     return true;
 }
 
-bool str_eq_str(str_number s, str_number t)
+
+bool
+str_eq_str(str_number s, str_number t)
 {
     pool_pointer j, k;
+
     if (length(s) != length(t))
         return false;
-    if ((length(s) == 1)) {
+
+    if (length(s) == 1) {
         if (s < 65536L) {
             if (t < 65536L) {
                 if (s != t)
                     return false;
             } else {
-
-                if (s != str_pool[str_start[(t) - 65536L]])
+                if (s != str_pool[str_start[t - 65536L]])
                     return false;
             }
         } else {
-
             if (t < 65536L) {
-                if (str_pool[str_start[(s) - 65536L]] != t)
+                if (str_pool[str_start[s - 65536L]] != t)
                     return false;
             } else {
-
-                if (str_pool[str_start[(s) - 65536L]] != str_pool[str_start[(t) - 65536L]])
+                if (str_pool[str_start[s - 65536L]] != str_pool[str_start[t - 65536L]])
                     return false;
             }
         }
     } else {
+        j = str_start[s - 65536L];
+        k = str_start[t - 65536L];
 
-        j = str_start[(s) - 65536L];
-        k = str_start[(t) - 65536L];
-        while (j < str_start[(s + 1) - 65536L]) {
-
+        while (j < str_start[s + 1 - 65536L]) {
             if (str_pool[j] != str_pool[k])
                 return false;
+
             j++;
             k++;
         }
     }
+
     return true;
 }
 
-str_number search_string(str_number search)
+
+str_number
+search_string(str_number search)
 {
     str_number s;
     int32_t len;
+
     len = length(search);
+
     if (len == 0) {
         return EMPTY_STRING;
     } else {
-
         s = search - 1;
+
         while (s > 65535L) {
-
             if (length(s) == len) {
-
                 if (str_eq_str(s, search)) {
                     return s;
                 }
             }
+
             s--;
         }
     }
+
     return 0;
 }
 
-str_number slow_make_string(void)
+
+str_number
+slow_make_string(void)
 {
     str_number s;
     str_number t;
+
     t = make_string();
     s = search_string(t);
+
     if (s > 0) {
-        {
-            str_ptr--;
-            pool_ptr = str_start[str_ptr - TOO_BIG_CHAR];
-        }
+        str_ptr--;
+        pool_ptr = str_start[str_ptr - TOO_BIG_CHAR];
         return s;
     }
+
     return t;
 }
