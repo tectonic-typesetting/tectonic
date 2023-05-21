1dfda65050a143b710ae7a8d4c616e14ac9ef2f6
Rune Tynan <runetynan@gmail.com>
2023-05-21 13:59:34 -0400

    Fix clippy

--- src/bin/tectonic/v2cli.rs
+++ src/bin/tectonic/v2cli.rs
@@ -550,7 +550,7 @@ impl WatchCommand {
                 let cmd = Command::Shell {
                     shell: shell.clone(),
                     args: vec![],
-                    command: format!("{exe_name} -X {}", x.to_string()),
+                    command: format!("{exe_name} -X {}", x),
                 };
                 cmds.push(cmd)
             }
