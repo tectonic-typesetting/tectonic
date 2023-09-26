069da643d9f15c9b9ddd2966e70f4d8f6ecbcd87
Rune Tynan <runetynan@gmail.com>
2023-09-26 13:25:42 -0400

    Don't bother boxing peekable

--- crates/engine_bibtex/src/log.rs
+++ crates/engine_bibtex/src/log.rs
@@ -580,7 +580,7 @@ pub(crate) fn bst_err_print_and_look_for_blank_line(
     bst_ln_num_print(ctx, pool)?;
     print_bad_input_line(buffers);
     while buffers.init(BufTy::Base) != 0 {
-        if !input_ln(ctx.bst_file.as_deref_mut(), buffers) {
+        if !input_ln(ctx.bst_file.as_mut(), buffers) {
             return Err(BibtexError::Recover);
         } else {
             ctx.bst_line_num += 1;
