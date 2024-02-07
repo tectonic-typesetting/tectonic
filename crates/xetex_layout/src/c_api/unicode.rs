db89198571cf7897cc5ae62b9ad7f2cf5a8cace6
Rune Tynan <runetynan@gmail.com>
2024-02-06 19:50:04 -0800

    Continue port. FontManagerBackend functionality mostly implemented for FC

--- crates/xetex_layout/src/c_api/unicode.rs
+++ crates/xetex_layout/src/c_api/unicode.rs
@@ -1,6 +1,7 @@
 #![allow(nonstandard_style)]
 
 pub type UErrorCode = libc::c_int;
+pub type UChar = u16;
 
 pub const U_ZERO_ERROR: UErrorCode = 0;
 
@@ -14,4 +15,20 @@ pub struct UConverter(());
 extern "C" {
     pub fn ucnv_open(name: *const libc::c_char, err: *mut UErrorCode) -> *mut UConverter;
     pub fn ucnv_close(conv: *mut UConverter);
+    pub fn ucnv_toUChars(
+        conv: *mut UConverter,
+        dest: *mut UChar,
+        dest_capacity: i32,
+        src: *const libc::c_char,
+        src_len: i32,
+        p_error_code: *mut UErrorCode,
+    ) -> i32;
+    pub fn ucnv_fromUChars(
+        conv: *mut UConverter,
+        dest: *mut libc::c_char,
+        dest_capacity: i32,
+        src: *const UChar,
+        src_len: i32,
+        p_error_code: *mut UErrorCode,
+    ) -> i32;
 }
