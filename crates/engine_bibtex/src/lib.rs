53a0237137b392ba24c8495a9f8b12dd12171072
Rune Tynan <runetynan@gmail.com>
2024-02-20 16:38:37 -0800

    Fix not closing files when spotless history is returned

--- crates/engine_bibtex/src/lib.rs
+++ crates/engine_bibtex/src/lib.rs
@@ -333,9 +333,7 @@ pub(crate) fn bibtex_main(ctx: &mut Bibtex<'_, '_>, aux_file_name: &CStr) -> His
 
     let res = inner_bibtex_main(ctx, &mut globals, aux_file_name);
     match res {
-        Ok(History::Spotless) => (),
-        Ok(hist) => return hist,
-        Err(BibtexError::Recover) => {
+        Err(BibtexError::Recover) | Ok(History::Spotless) => {
             // SAFETY: bst_file guaranteed valid at this point
             unsafe { peekable_close(ctx, ctx.bst_file) };
             ctx.bst_file = None;
@@ -345,6 +343,7 @@ pub(crate) fn bibtex_main(ctx: &mut Bibtex<'_, '_>, aux_file_name: &CStr) -> His
             ttbc_output_close(ctx.engine, ctx.bbl_file);
         }
         Err(BibtexError::Fatal) => (),
+        Ok(hist) => return hist,
     }
 
     match get_history() {
