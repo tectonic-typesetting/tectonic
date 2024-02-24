020ca424e1a82a0ff76733aa4ed5de13737d3160
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-02-24 13:25:47 -0800

    Tweak C function signatures & autoformat

--- crates/bridge_core/support/support.c
+++ crates/bridge_core/support/support.c
@@ -1,16 +1,14 @@
 /* Copyright 2017-2021 the Tectonic Project
  * Licensed under the MIT License.
-*/
+ */
 
 #include "tectonic_bridge_core.h"
 
 #include <setjmp.h>
 #include <stdio.h> /*vsnprintf*/
 
-
 #define BUF_SIZE 1024
 static char format_buf[BUF_SIZE] = "";
-
 
 /* The memory management utilities. */
 
@@ -21,11 +19,10 @@ xcalloc(size_t nelem, size_t elsize)
 
     if (new_mem == NULL)
         _tt_abort("xcalloc request for %lu elements of size %lu failed",
-                  (unsigned long) nelem, (unsigned long) elsize);
+                  (unsigned long)nelem, (unsigned long)elsize);
 
     return new_mem;
 }
-
 
 void *
 xmalloc(size_t size)
@@ -33,49 +30,48 @@ xmalloc(size_t size)
     void *new_mem = malloc(size ? size : 1);
 
     if (new_mem == NULL)
-        _tt_abort("xmalloc request for %lu bytes failed", (unsigned long) size);
+        _tt_abort("xmalloc request for %lu bytes failed", (unsigned long)size);
 
     return new_mem;
 }
-
 
 void *
 xrealloc(void *old_ptr, size_t size)
 {
     void *new_mem;
 
-    if (old_ptr == NULL) {
+    if (old_ptr == NULL)
+    {
         new_mem = xmalloc(size);
-    } else {
+    }
+    else
+    {
         new_mem = realloc(old_ptr, size ? size : 1);
         if (new_mem == NULL)
-            _tt_abort("xrealloc() to %lu bytes failed", (unsigned long) size);
+            _tt_abort("xrealloc() to %lu bytes failed", (unsigned long)size);
     }
 
     return new_mem;
 }
 
-
 char *
 xstrdup(const char *s)
 {
-    char *new_string = xmalloc(strlen (s) + 1);
+    char *new_string = xmalloc(strlen(s) + 1);
     return strcpy(new_string, s);
 }
 
-
 /* C API helpers that don't rely upon the global state variables */
 
-PRINTF_FUNC(2,0) void
-ttstub_diag_vprintf(ttbc_diagnostic_t *diag, const char *format, va_list ap)
+PRINTF_FUNC(2, 0)
+void ttstub_diag_vprintf(ttbc_diagnostic_t *diag, const char *format, va_list ap)
 {
     vsnprintf(format_buf, BUF_SIZE, format, ap);
     ttbc_diag_append(diag, format_buf);
 }
 
-
-PRINTF_FUNC(2,3) void
-ttstub_diag_printf(ttbc_diagnostic_t *diag, const char *format, ...)
+PRINTF_FUNC(2, 3)
+void ttstub_diag_printf(ttbc_diagnostic_t *diag, const char *format, ...)
 {
     va_list ap;
 
@@ -84,15 +80,12 @@ ttstub_diag_printf(ttbc_diagnostic_t *diag, const char *format, ...)
     va_end(ap);
 }
 
-
 /* The global state helpers */
 
 static ttbc_state_t *tectonic_global_bridge_core = NULL;
 static jmp_buf jump_buffer;
 
-
-NORETURN PRINTF_FUNC(1,2) int
-_tt_abort(const char *format, ...)
+NORETURN PRINTF_FUNC(1, 2) int _tt_abort(const char *format, ...)
 {
     va_list ap;
 
@@ -101,7 +94,6 @@ _tt_abort(const char *format, ...)
     va_end(ap);
     longjmp(jump_buffer, 1);
 }
-
 
 /* This function is referenced on the Rust side to get, well, the error message,
  * when there's an abort. `format_buf` is used in other places so this it's only
@@ -112,7 +104,6 @@ _ttbc_get_error_message(void)
     return format_buf;
 }
 
-
 jmp_buf *
 ttbc_global_engine_enter(ttbc_state_t *api)
 {
@@ -120,44 +111,37 @@ ttbc_global_engine_enter(ttbc_state_t *api)
     return &jump_buffer;
 }
 
-
-void
-ttbc_global_engine_exit(void)
+void ttbc_global_engine_exit(void)
 {
     tectonic_global_bridge_core = NULL;
 }
 
-
-PRINTF_FUNC(1,2) void
-ttstub_issue_warning(const char *format, ...)
+PRINTF_FUNC(1, 2)
+void ttstub_issue_warning(const char *format, ...)
 {
     va_list ap;
 
     va_start(ap, format);
     vsnprintf(format_buf, BUF_SIZE, format, ap);
     va_end(ap);
-    ttbc_issue_warning(tectonic_global_bridge_core, format_buf);
+    ttbc_issue_warning(format_buf);
 }
 
-
-PRINTF_FUNC(1,2) void
-ttstub_issue_error(const char *format, ...)
+PRINTF_FUNC(1, 2)
+void ttstub_issue_error(const char *format, ...)
 {
     va_list ap;
 
     va_start(ap, format);
     vsnprintf(format_buf, BUF_SIZE, format, ap); /* Not ideal to (ab)use format_buf here */
     va_end(ap);
-    ttbc_issue_error(tectonic_global_bridge_core, format_buf);
+    ttbc_issue_error(format_buf);
 }
 
-
-void
-ttstub_diag_finish(ttbc_diagnostic_t *diag)
+void ttstub_diag_finish(ttbc_diagnostic_t *diag)
 {
-    ttbc_diag_finish(tectonic_global_bridge_core, diag);
+    ttbc_diag_finish(diag);
 }
-
 
 rust_output_handle_t
 ttstub_output_open(char const *path, int is_gz)
@@ -165,30 +149,25 @@ ttstub_output_open(char const *path, int is_gz)
     return ttbc_output_open(tectonic_global_bridge_core, path, is_gz);
 }
 
-
 rust_output_handle_t
 ttstub_output_open_stdout(void)
 {
     return ttbc_output_open_stdout(tectonic_global_bridge_core);
 }
 
-
-int
-ttstub_output_putc(rust_output_handle_t handle, int c)
+int ttstub_output_putc(rust_output_handle_t handle, int c)
 {
     return ttbc_output_putc(tectonic_global_bridge_core, handle, c);
 }
 
-
 size_t
 ttstub_output_write(rust_output_handle_t handle, const char *data, size_t len)
 {
-    return ttbc_output_write(tectonic_global_bridge_core, handle, (const uint8_t*) data, len);
+    return ttbc_output_write(tectonic_global_bridge_core, handle, (const uint8_t *)data, len);
 }
 
-
-PRINTF_FUNC(2,3) int
-ttstub_fprintf(rust_output_handle_t handle, const char *format, ...)
+PRINTF_FUNC(2, 3)
+int ttstub_fprintf(rust_output_handle_t handle, const char *format, ...)
 {
     static char fprintf_buf[BUF_SIZE] = "";
     va_list ap;
@@ -197,32 +176,29 @@ ttstub_fprintf(rust_output_handle_t handle, const char *format, ...)
     int len = vsnprintf(fprintf_buf, BUF_SIZE, format, ap);
     va_end(ap);
 
-    if (len >= BUF_SIZE) {
+    if (len >= BUF_SIZE)
+    {
         len = BUF_SIZE - 1;
         fprintf_buf[len] = '\0';
     }
 
-    if (len >= 0) {
+    if (len >= 0)
+    {
         ttstub_output_write(handle, fprintf_buf, len);
     }
 
     return len;
 }
 
-
-int
-ttstub_output_flush(rust_output_handle_t handle)
+int ttstub_output_flush(rust_output_handle_t handle)
 {
     return ttbc_output_flush(tectonic_global_bridge_core, handle);
 }
 
-
-int
-ttstub_output_close(rust_output_handle_t handle)
+int ttstub_output_close(rust_output_handle_t handle)
 {
     return ttbc_output_close(tectonic_global_bridge_core, handle);
 }
-
 
 rust_input_handle_t
 ttstub_input_open(char const *path, ttbc_file_format format, int is_gz)
@@ -230,18 +206,16 @@ ttstub_input_open(char const *path, ttbc_file_format format, int is_gz)
     return ttbc_input_open(tectonic_global_bridge_core, path, format, is_gz);
 }
 
-
 rust_input_handle_t
 ttstub_input_open_primary(void)
 {
     return ttbc_input_open_primary(tectonic_global_bridge_core);
 }
 
-
 ssize_t
 ttstub_get_last_input_abspath(char *buffer, size_t len)
 {
-    return ttbc_get_last_input_abspath(tectonic_global_bridge_core, (uint8_t *) buffer, len);
+    return ttbc_get_last_input_abspath(tectonic_global_bridge_core, (uint8_t *)buffer, len);
 }
 
 size_t
@@ -249,7 +223,6 @@ ttstub_input_get_size(rust_input_handle_t handle)
 {
     return ttbc_input_get_size(tectonic_global_bridge_core, handle);
 }
-
 
 time_t
 ttstub_input_get_mtime(rust_input_handle_t handle)
@@ -259,9 +232,8 @@ ttstub_input_get_mtime(rust_input_handle_t handle)
      * guarantees about what the type time_t actually is. So let's just cast and
      * hope for the best. */
     int64_t ti = ttbc_input_get_mtime(tectonic_global_bridge_core, handle);
-    return (time_t) ti;
+    return (time_t)ti;
 }
-
 
 size_t
 ttstub_input_seek(rust_input_handle_t handle, ssize_t offset, int whence)
@@ -270,7 +242,8 @@ ttstub_input_seek(rust_input_handle_t handle, ssize_t offset, int whence)
 
     size_t rv = ttbc_input_seek(tectonic_global_bridge_core, handle, offset, whence, &internal_error);
 
-    if (internal_error) {
+    if (internal_error)
+    {
         // Nonzero indicates a serious internal error.
         longjmp(jump_buffer, 1);
     }
@@ -278,32 +251,26 @@ ttstub_input_seek(rust_input_handle_t handle, ssize_t offset, int whence)
     return rv;
 }
 
-
 ssize_t
 ttstub_input_read(rust_input_handle_t handle, char *data, size_t len)
 {
-    return ttbc_input_read(tectonic_global_bridge_core, handle, (uint8_t *) data, len);
+    return ttbc_input_read(tectonic_global_bridge_core, handle, (uint8_t *)data, len);
 }
 
-
-int
-ttstub_input_getc(rust_input_handle_t handle)
+int ttstub_input_getc(rust_input_handle_t handle)
 {
     return ttbc_input_getc(tectonic_global_bridge_core, handle);
 }
 
-
-int
-ttstub_input_ungetc(rust_input_handle_t handle, int ch)
+int ttstub_input_ungetc(rust_input_handle_t handle, int ch)
 {
     return ttbc_input_ungetc(tectonic_global_bridge_core, handle, ch);
 }
 
-
-int
-ttstub_input_close(rust_input_handle_t handle)
+int ttstub_input_close(rust_input_handle_t handle)
 {
-    if (ttbc_input_close(tectonic_global_bridge_core, handle)) {
+    if (ttbc_input_close(tectonic_global_bridge_core, handle))
+    {
         // Nonzero return value indicates a serious internal error.
         longjmp(jump_buffer, 1);
     }
@@ -311,15 +278,12 @@ ttstub_input_close(rust_input_handle_t handle)
     return 0;
 }
 
-
-int
-ttstub_get_file_md5(char const *path, char *digest)
+int ttstub_get_file_md5(char const *path, char *digest)
 {
-    return ttbc_get_file_md5(tectonic_global_bridge_core, path, (uint8_t *) digest);
+    return ttbc_get_file_md5(tectonic_global_bridge_core, path, (uint8_t *)digest);
 }
 
-int
-ttstub_shell_escape(const unsigned short *cmd, size_t len)
+int ttstub_shell_escape(const unsigned short *cmd, size_t len)
 {
     return ttbc_shell_escape(tectonic_global_bridge_core, cmd, len);
 }
--- crates/bridge_core/support/tectonic_bridge_core_generated.h
+++ crates/bridge_core/support/tectonic_bridge_core_generated.h
@@ -10,7 +10,6 @@ typedef struct ttbc_output_handle_t ttbc_output_handle_t;
 typedef ttbc_input_handle_t *rust_input_handle_t;
 typedef ttbc_output_handle_t *rust_output_handle_t;
 
-
 /**
  * Different types of files that can be opened by TeX engines
  *
@@ -18,7 +17,8 @@ typedef ttbc_output_handle_t *rust_output_handle_t;
  * for a file to open.
  *
  */
-typedef enum {
+typedef enum
+{
   /**
    * An Adobe Font Metrics file.
    */
@@ -132,207 +132,208 @@ typedef struct ttbc_state_t ttbc_state_t;
 typedef struct ttbc_diagnostic_t ttbc_diagnostic_t;
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif // __cplusplus
 
-extern const char *_ttbc_get_error_message(void);
+  extern const char *_ttbc_get_error_message(void);
 
-/**
- * Issue a warning.
- *
- * # Safety
- *
- * This function is unsafe because it accepts a raw C string.
- */
-void ttbc_issue_warning(ttbc_state_t *es, const char *text);
+  /**
+   * Issue a warning.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it accepts a raw C string.
+   */
+  void ttbc_issue_warning(const char *text);
 
-/**
- * Issue an error.
- *
- * # Safety
- *
- * This function is unsafe because it accepts a raw C string.
- */
-void ttbc_issue_error(ttbc_state_t *es, const char *text);
+  /**
+   * Issue an error.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it accepts a raw C string.
+   */
+  void ttbc_issue_error(const char *text);
 
-/**
- * Calculate the MD5 digest of a Tectonic file.
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw pointers from C.
- */
-int ttbc_get_file_md5(ttbc_state_t *es, const char *path, uint8_t *digest);
+  /**
+   * Calculate the MD5 digest of a Tectonic file.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw pointers from C.
+   */
+  int ttbc_get_file_md5(ttbc_state_t *es, const char *path, uint8_t *digest);
 
-/**
- * Calculate the MD5 digest of a block of binary data.
- *
- * This actually doesn't rely on the state and isn't really I/O, but we also
- * have a get-file-MD5 routine so it's convenient to have this here.
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw pointers from C.
- */
-int ttbc_get_data_md5(const uint8_t *data, size_t len, uint8_t *digest);
+  /**
+   * Calculate the MD5 digest of a block of binary data.
+   *
+   * This actually doesn't rely on the state and isn't really I/O, but we also
+   * have a get-file-MD5 routine so it's convenient to have this here.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw pointers from C.
+   */
+  int ttbc_get_data_md5(const uint8_t *data, size_t len, uint8_t *digest);
 
-/**
- * Open a Tectonic file for output.
- *
- * # Safety
- *
- * This function is unsafe because it accepts a raw C string.
- */
-ttbc_output_handle_t *ttbc_output_open(ttbc_state_t *es, const char *name, int is_gz);
+  /**
+   * Open a Tectonic file for output.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it accepts a raw C string.
+   */
+  ttbc_output_handle_t *ttbc_output_open(ttbc_state_t *es, const char *name, int is_gz);
 
-/**
- * Open the general user output stream as a Tectonic output file.
- */
-ttbc_output_handle_t *ttbc_output_open_stdout(ttbc_state_t *es);
+  /**
+   * Open the general user output stream as a Tectonic output file.
+   */
+  ttbc_output_handle_t *ttbc_output_open_stdout(ttbc_state_t *es);
 
-/**
- * Write a single character to a Tectonic output file.
- */
-int ttbc_output_putc(ttbc_state_t *es, ttbc_output_handle_t *handle, int c);
+  /**
+   * Write a single character to a Tectonic output file.
+   */
+  int ttbc_output_putc(ttbc_state_t *es, ttbc_output_handle_t *handle, int c);
 
-/**
- * Write data to a Tectonic output file.
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw C pointers.
- */
-size_t ttbc_output_write(ttbc_state_t *es,
-                         ttbc_output_handle_t *handle,
-                         const uint8_t *data,
-                         size_t len);
+  /**
+   * Write data to a Tectonic output file.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw C pointers.
+   */
+  size_t ttbc_output_write(ttbc_state_t *es,
+                           ttbc_output_handle_t *handle,
+                           const uint8_t *data,
+                           size_t len);
 
-/**
- * Flush pending writes to a Tectonic output file.
- */
-int ttbc_output_flush(ttbc_state_t *es, ttbc_output_handle_t *handle);
+  /**
+   * Flush pending writes to a Tectonic output file.
+   */
+  int ttbc_output_flush(ttbc_state_t *es, ttbc_output_handle_t *handle);
 
-/**
- * Close a Tectonic output file.
- */
-int ttbc_output_close(ttbc_state_t *es, ttbc_output_handle_t *handle);
+  /**
+   * Close a Tectonic output file.
+   */
+  int ttbc_output_close(ttbc_state_t *es, ttbc_output_handle_t *handle);
 
-/**
- * Open a Tectonic file for input.
- *
- * # Safety
- *
- * This function is unsafe because it accepts a raw C string.
- */
-ttbc_input_handle_t *ttbc_input_open(ttbc_state_t *es,
-                                     const char *name,
-                                     ttbc_file_format format,
-                                     int is_gz);
+  /**
+   * Open a Tectonic file for input.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it accepts a raw C string.
+   */
+  ttbc_input_handle_t *ttbc_input_open(ttbc_state_t *es,
+                                       const char *name,
+                                       ttbc_file_format format,
+                                       int is_gz);
 
-/**
- * Open the "primary input" file.
- */
-ttbc_input_handle_t *ttbc_input_open_primary(ttbc_state_t *es);
+  /**
+   * Open the "primary input" file.
+   */
+  ttbc_input_handle_t *ttbc_input_open_primary(ttbc_state_t *es);
 
-/**
- * Get the filesystem path of the most-recently-opened input file.
- *
- * This function is needed by SyncTeX, because its output file should contain
- * absolute filesystem paths to the input source files. In principle this
- * functionality could be implemented in a few different ways, but the approach
- * used here is the most backward-compatible. This function will fill in the
- * caller's buffer with the filesystem path associated with the most
- * recently-opened input file, including a terminating NUL, if possible.
- *
- * It returns 0 if no such path is known, -1 if the path cannot be expressed
- * UTF-8, -2 if the destination buffer is not big enough, or the number of
- * bytes written into the buffer (including a terminating NUL) otherwise.
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw C pointers.
- */
-ssize_t ttbc_get_last_input_abspath(ttbc_state_t *es, uint8_t *buffer, size_t len);
+  /**
+   * Get the filesystem path of the most-recently-opened input file.
+   *
+   * This function is needed by SyncTeX, because its output file should contain
+   * absolute filesystem paths to the input source files. In principle this
+   * functionality could be implemented in a few different ways, but the approach
+   * used here is the most backward-compatible. This function will fill in the
+   * caller's buffer with the filesystem path associated with the most
+   * recently-opened input file, including a terminating NUL, if possible.
+   *
+   * It returns 0 if no such path is known, -1 if the path cannot be expressed
+   * UTF-8, -2 if the destination buffer is not big enough, or the number of
+   * bytes written into the buffer (including a terminating NUL) otherwise.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw C pointers.
+   */
+  ssize_t ttbc_get_last_input_abspath(ttbc_state_t *es, uint8_t *buffer, size_t len);
 
-/**
- * Get the size of a Tectonic input file.
- */
-size_t ttbc_input_get_size(ttbc_state_t *es, ttbc_input_handle_t *handle);
+  /**
+   * Get the size of a Tectonic input file.
+   */
+  size_t ttbc_input_get_size(ttbc_state_t *es, ttbc_input_handle_t *handle);
 
-/**
- * Get the modification time of a Tectonic input file.
- */
-int64_t ttbc_input_get_mtime(ttbc_state_t *es, ttbc_input_handle_t *handle);
+  /**
+   * Get the modification time of a Tectonic input file.
+   */
+  int64_t ttbc_input_get_mtime(ttbc_state_t *es, ttbc_input_handle_t *handle);
 
-/**
- * Seek in a Tectonic input stream.
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw pointers from C.
- */
-size_t ttbc_input_seek(ttbc_state_t *es,
-                       ttbc_input_handle_t *handle,
-                       ssize_t offset,
-                       int whence,
-                       int *internal_error);
+  /**
+   * Seek in a Tectonic input stream.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw pointers from C.
+   */
+  size_t ttbc_input_seek(ttbc_state_t *es,
+                         ttbc_input_handle_t *handle,
+                         ssize_t offset,
+                         int whence,
+                         int *internal_error);
 
-/**
- * Get a single character from a Tectonic input file.
- */
-int ttbc_input_getc(ttbc_state_t *es, ttbc_input_handle_t *handle);
+  /**
+   * Get a single character from a Tectonic input file.
+   */
+  int ttbc_input_getc(ttbc_state_t *es, ttbc_input_handle_t *handle);
 
-/**
- * Put back a character that was obtained from a `getc` call.
- */
-int ttbc_input_ungetc(ttbc_state_t *es, ttbc_input_handle_t *handle, int ch);
+  /**
+   * Put back a character that was obtained from a `getc` call.
+   */
+  int ttbc_input_ungetc(ttbc_state_t *es, ttbc_input_handle_t *handle, int ch);
 
-/**
- * Read data from a Tectonic input handle
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw C pointers.
- */
-ssize_t ttbc_input_read(ttbc_state_t *es, ttbc_input_handle_t *handle, uint8_t *data, size_t len);
+  /**
+   * Read data from a Tectonic input handle
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw C pointers.
+   */
+  ssize_t ttbc_input_read(ttbc_state_t *es, ttbc_input_handle_t *handle, uint8_t *data, size_t len);
 
-/**
- * Close a Tectonic input file.
- */
-int ttbc_input_close(ttbc_state_t *es, ttbc_input_handle_t *handle);
+  /**
+   * Close a Tectonic input file.
+   */
+  int ttbc_input_close(ttbc_state_t *es, ttbc_input_handle_t *handle);
 
-/**
- * Create a new diagnostic that will be reported as a warning.
- */
-ttbc_diagnostic_t *ttbc_diag_begin_warning(void);
+  /**
+   * Create a new diagnostic that will be reported as a warning.
+   */
+  ttbc_diagnostic_t *ttbc_diag_begin_warning(void);
 
-/**
- * Create a new diagnostic that will be reported as an error.
- */
-ttbc_diagnostic_t *ttbc_diag_begin_error(void);
+  /**
+   * Create a new diagnostic that will be reported as an error.
+   */
+  ttbc_diagnostic_t *ttbc_diag_begin_error(void);
 
-/**
- * Append text to a diagnostic.
- *
- * # Safety
- *
- * This function is unsafe because it accepts a raw C string.
- */
-void ttbc_diag_append(ttbc_diagnostic_t *diag, const char *text);
+  /**
+   * Append text to a diagnostic.
+   *
+   * # Safety
+   *
+   * This function is unsafe because it accepts a raw C string.
+   */
+  void ttbc_diag_append(ttbc_diagnostic_t *diag, const char *text);
 
-/**
- * "Finish" a diagnostic: report it to the driver and free the diagnostic object.
- */
-void ttbc_diag_finish(ttbc_state_t *es, ttbc_diagnostic_t *diag);
+  /**
+   * "Finish" a diagnostic: report it to the driver and free the diagnostic object.
+   */
+  void ttbc_diag_finish(ttbc_diagnostic_t *diag);
 
-/**
- * Run a shell command
- *
- * # Safety
- *
- * This function is unsafe because it dereferences raw pointers from C and accepts a raw C string.
- */
-int ttbc_shell_escape(ttbc_state_t *es, const uint16_t *cmd, size_t len);
+  /**
+   * Run a shell command
+   *
+   * # Safety
+   *
+   * This function is unsafe because it dereferences raw pointers from C and accepts a raw C string.
+   */
+  int ttbc_shell_escape(ttbc_state_t *es, const uint16_t *cmd, size_t len);
 
 #ifdef __cplusplus
 } // extern "C"
