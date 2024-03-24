7f10fd763e97f2f3703f08b78a09b83085dce98e
Rune Tynan <runetynan@gmail.com>
2024-03-24 16:18:07 -0700

    Port FontFuncs

--- crates/bridge_harfbuzz/src/lib.rs
+++ crates/bridge_harfbuzz/src/lib.rs
@@ -6,18 +6,26 @@
 
 #![allow(non_camel_case_types)]
 
+use std::marker::PhantomData;
 use std::ptr;
 
 pub mod sys;
 
-pub use sys::hb_tag_t as Tag;
+pub use sys::hb_codepoint_t as Codepoint;
+pub use sys::hb_glyph_extents_t as GlyphExtents;
 pub use sys::hb_memory_mode_t as MemoryMode;
+pub use sys::hb_position_t as Position;
+pub use sys::hb_tag_t as Tag;
 
 /// Import something from our bridge crates so that we ensure that we actually
 /// link with them, to pull in the symbols defined in the C APIs.
 mod linkage {
     #[allow(unused_imports)]
     use tectonic_bridge_graphite2 as clippyrenamehack1;
+}
+
+unsafe extern "C" fn dealloc<T: 'static>(user_data: *mut ()) {
+    let _ = unsafe { Box::from_raw(user_data.cast::<T>()) };
 }
 
 pub struct Blob(*mut sys::hb_blob_t);
@@ -46,7 +54,11 @@ pub struct Face(*mut sys::hb_face_t);
 
 impl Face {
     pub fn new_tables<T: Fn(Face, Tag) -> Option<Blob> + 'static>(f: T) -> Face {
-        unsafe extern "C" fn get_table<T: Fn(Face, Tag) -> Option<Blob> + 'static>(face: *mut sys::hb_face_t, tag: sys::hb_tag_t, user_data: *mut ()) -> *mut sys::hb_blob_t {
+        unsafe extern "C" fn get_table<T: Fn(Face, Tag) -> Option<Blob> + 'static>(
+            face: *mut sys::hb_face_t,
+            tag: sys::hb_tag_t,
+            user_data: *mut (),
+        ) -> *mut sys::hb_blob_t {
             let f = unsafe { &*user_data.cast::<T>() };
             let face = Face(face);
             match f(face, tag) {
@@ -55,17 +67,23 @@ impl Face {
             }
         }
 
-        unsafe extern "C" fn dealloc<T: Fn(Face, Tag) -> Option<Blob> + 'static>(user_data: *mut ()) {
-            let _ = unsafe { Box::from_raw(user_data.cast::<T>()) };
-        }
-
-        let face = unsafe { sys::hb_face_create_for_tables(
-            get_table::<T>,
-            Box::into_raw(Box::new(f)).cast(),
-            Some(dealloc::<T>),
-        ) };
+        let face = unsafe {
+            sys::hb_face_create_for_tables(
+                get_table::<T>,
+                Box::into_raw(Box::new(f)).cast(),
+                Some(dealloc::<T>),
+            )
+        };
 
         Face(face)
+    }
+
+    pub fn set_index(&mut self, index: u32) {
+        unsafe { sys::hb_face_set_index(self.0, index as libc::c_uint) }
+    }
+
+    pub fn set_upem(&mut self, upem: u32) {
+        unsafe { sys::hb_face_set_upem(self.0, upem as libc::c_uint) }
     }
 }
 
@@ -85,6 +103,29 @@ impl Font {
     pub fn get_face(&self) -> Face {
         Face(unsafe { sys::hb_font_get_face(self.0) })
     }
+
+    pub fn set_scale(&mut self, x: i32, y: i32) {
+        unsafe { sys::hb_font_set_scale(self.0, x as libc::c_int, y as libc::c_int) }
+    }
+
+    pub fn set_ppem(&mut self, x: u32, y: u32) {
+        unsafe { sys::hb_font_set_ppem(self.0, x as libc::c_uint, y as libc::c_uint) }
+    }
+
+    pub fn set_funcs<T: 'static>(&mut self, funcs: FontFuncs<T>, data: T)
+    where
+        T: 'static,
+    {
+        let funcs = funcs.into_raw();
+        unsafe {
+            sys::hb_font_set_funcs(
+                self.0,
+                funcs,
+                Box::into_raw(Box::new(data)).cast(),
+                Some(dealloc::<T>),
+            )
+        }
+    }
 }
 
 impl Drop for Font {
@@ -92,6 +133,11 @@ impl Drop for Font {
         unsafe { sys::hb_font_destroy(self.0) }
     }
 }
+
+mod font_funcs;
+
+pub use font_funcs::FontFuncs;
+use font_funcs::*;
 
 #[test]
 fn linkage() {}
