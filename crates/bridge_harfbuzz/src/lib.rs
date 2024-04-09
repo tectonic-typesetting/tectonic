4bf5ce624844298d46423960fb7ec5a5d224d0c0
Rune Tynan <runetynan@gmail.com>
2024-04-08 21:57:58 -0700

    Add Graphite feature test and fix feature selection by 4-letter code.

--- crates/bridge_harfbuzz/src/lib.rs
+++ crates/bridge_harfbuzz/src/lib.rs
@@ -60,6 +60,10 @@ impl Tag {
         Tag(val)
     }
 
+    pub fn from_str(val: &str) -> Tag {
+        Tag(unsafe { sys::hb_tag_from_string(val.as_ptr().cast(), val.len() as libc::c_int) })
+    }
+
     pub fn from_cstr(val: &CStr) -> Tag {
         Tag(unsafe { sys::hb_tag_from_string(val.as_ptr(), -1) })
     }
