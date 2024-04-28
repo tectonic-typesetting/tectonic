e874aba89d6ded631aa0cf822e61b530c53d2e34
CraftSpider <runetynan@gmail.com>
2024-04-28 13:19:23 -0700

    Fixup build scripts

--- crates/xetex_layout/build.rs
+++ crates/xetex_layout/build.rs
@@ -9,33 +9,12 @@
 //! Windows, we use Fontconfig to discover fonts.
 
 use std::{env, path::PathBuf};
-use tectonic_cfg_support::target_cfg;
 
 fn main() {
-    // let target = env::var("TARGET").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
     let manifest_dir: PathBuf = env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    let is_mac_os = target_cfg!(target_os = "macos");
 
-    // Include paths and settings exported by our internal dependencies.
-
-    // TODO: Don't unwrap, won't exist on mac
-    let fontconfig_include_path = env::var("DEP_FONTCONFIG_INCLUDE_PATH").unwrap();
-    let freetype2_include_path = env::var("DEP_FREETYPE2_INCLUDE_PATH").unwrap();
-    let graphite2_include_path = env::var("DEP_GRAPHITE2_INCLUDE_PATH").unwrap();
-    let harfbuzz_include_path = env::var("DEP_HARFBUZZ_INCLUDE_PATH").unwrap();
-    let icu_include_path = env::var("DEP_ICUUC_INCLUDE_PATH").unwrap();
-
-    if is_mac_os {
-        println!("cargo:rustc-link-lib=framework=Foundation");
-        println!("cargo:rustc-link-lib=framework=CoreFoundation");
-        println!("cargo:rustc-link-lib=framework=CoreGraphics");
-        println!("cargo:rustc-link-lib=framework=CoreText");
-        println!("cargo:rustc-link-lib=framework=AppKit");
-    }
-
-    // OK, back to generic build rules.
-    // Copy the static header file for C preprocessing convenience.
+    // Copy the generated header file for exported functions.
 
     let mut main_header_src = manifest_dir.clone();
     main_header_src.push("layout");
@@ -47,33 +26,5 @@ fn main() {
     std::fs::copy(&main_header_src, &main_header_dest).expect("failed to copy main header");
 
     println!("cargo:rerun-if-changed=layout/tectonic_xetex_layout.h");
-
-    // Cargo exposes this as the environment variable DEP_XXX_INCLUDE_PATH,
-    // where XXX is the "links" setting in Cargo.toml. This is the key element
-    // that allows us to have a network of crates containing both C/C++ and Rust
-    // code that all interlink.
-
-    print!("cargo:include-path={out_dir}");
-
-    for item in fontconfig_include_path.split(';') {
-        print!(";{item}");
-    }
-
-    for item in harfbuzz_include_path.split(';') {
-        print!(";{item}");
-    }
-
-    for item in freetype2_include_path.split(';') {
-        print!(";{item}");
-    }
-
-    for item in graphite2_include_path.split(';') {
-        print!(";{item}");
-    }
-
-    for item in icu_include_path.split(';') {
-        print!(";{item}");
-    }
-
-    println!();
+    println!("cargo:include-path={out_dir}");
 }
