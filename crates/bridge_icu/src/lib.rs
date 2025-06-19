e8b64ea709f321dafef1786ddc6f292a33a48867
Rune Tynan <runetynan@gmail.com>
2025-06-18 21:08:57 -0700

    Remove changes that shouldn't merge

--- crates/bridge_icu/src/lib.rs
+++ crates/bridge_icu/src/lib.rs
@@ -1,7 +1,5 @@
 // Copyright 2020 the Tectonic Project
 // Licensed under the MIT License.
 
-#![allow(nonstandard_style)]
-
 //! No Rust code. This crate exists to export the ICU *C* API into the
 //! Cargo framework.
