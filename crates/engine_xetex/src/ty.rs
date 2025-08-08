d8991f73c53560220a864430f4ba94d1f689b13b
CraftSpider <runetynan@gmail.com>
2025-08-08 12:52:37 -0700

    Port everything from scaledmath except m_log due to diagnostic dependency

---
+++ crates/engine_xetex/src/ty.rs
@@ -0,0 +1,1 @@
+pub type Scaled = i32;
--- crates/engine_xetex/xetex/xetex_bindings.h
+++ crates/engine_xetex/xetex/xetex_bindings.h
@@ -18,6 +18,8 @@
  */
 #define FORMAT_SERIAL 33
 
+typedef int32_t Scaled;
+
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
@@ -29,8 +31,46 @@ extern int tt_engine_xetex_main(ttbc_state_t *api,
                                 const char *input_file_name,
                                 uint64_t build_date);
 
-#ifdef __cplusplus
-} // extern "C"
-#endif // __cplusplus
+bool arith_error(void);
 
-#endif /* TECTONIC_ENGINE_XETEX_BINDGEN_H */
+void set_arith_error(bool val);
+
+Scaled tex_remainder(void);
+
+void set_tex_remainder(Scaled val);
+
+int32_t randoms(uintptr_t idx);
+
+uint8_t j_random(void);
+
+void set_j_random(uint8_t val);
+
+int32_t tex_round(double r);
+
+int32_t half(int32_t x);
+
+Scaled mult_and_add(int32_t n, Scaled x, Scaled y, Scaled max_answer);
+
+Scaled x_over_n(Scaled x, int32_t n);
+
+Scaled xn_over_d(Scaled x, int32_t n, int32_t d);
+
+Scaled round_xn_over_d(Scaled x, int32_t n, int32_t d);
+
+int32_t make_frac(int32_t p, int32_t q);
+
+int32_t take_frac(int32_t q, int32_t f);
+
+int32_t ab_vs_cd(int32_t a, int32_t b, int32_t c, int32_t d);
+
+void new_randoms(void);
+
+void init_randoms(int32_t seed);
+
+int32_t unif_rand(int32_t x);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  /* TECTONIC_ENGINE_XETEX_BINDGEN_H */
