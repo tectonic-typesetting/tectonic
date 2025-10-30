bebdf0690223ca0d05158872b3fe2e9485a7c898
CraftSpider <runetynan@gmail.com>
2025-10-30 14:10:35 -0700

    Port finalize_dvi_file

--- crates/engine_xetex/src/c_api/dvi.rs
+++ crates/engine_xetex/src/c_api/dvi.rs
@@ -1,9 +1,13 @@
 use crate::c_api::engine::{
-    rs_prepare_mag, IntPar, DEFINE_NATIVE_FONT, FNT_DEF1, POP, POST, TEX_INFINITY,
+    rs_prepare_mag, IntPar, DEFINE_NATIVE_FONT, EOP, FNT_DEF1, FONT_BASE, POP, POST, POST_POST,
+    TEX_INFINITY, TEX_NULL,
 };
 use crate::c_api::errors::fatal_error;
 use crate::c_api::font::{make_font_def, AAT_FONT_FLAG, OTGR_FONT_FLAG};
 use crate::c_api::globals::Globals;
+use crate::c_api::output::{
+    rs_print, rs_print_bytes, rs_print_char, rs_print_int, rs_print_nl_bytes,
+};
 use crate::c_api::pool::{length, rs_str_length};
 use crate::ty::StrNumber;
 use std::cell::RefCell;
@@ -14,6 +18,9 @@ use tectonic_bridge_core::OutputId;
 pub const DVI_BUF_SIZE: i32 = 16384;
 pub const HALF_BUF: i32 = DVI_BUF_SIZE / 2;
 pub const FNT_NUM_0: usize = 171; /* DVI code */
+
+pub const XDV_ID_BYTE: u8 = 7;
+pub const SPX_ID_BYTE: u8 = 100;
 
 thread_local! {
     pub static DVI_CTX: RefCell<DviCtx> = const { RefCell::new(DviCtx::new()) }
@@ -292,4 +299,112 @@ pub fn rs_dvi_font_def(globals: &mut Globals<'_, '_>, f: usize) {
 #[no_mangle]
 pub extern "C" fn dvi_font_def(f: usize) {
     Globals::with(|globals| rs_dvi_font_def(globals, f))
+}
+
+pub fn rs_finalize_dvi_file(globals: &mut Globals<'_, '_>) {
+    while globals.dvi.cur_s > -1 {
+        if globals.dvi.cur_s > 0 {
+            rs_dvi_out(globals, POP);
+        } else {
+            rs_dvi_out(globals, EOP);
+            globals.engine.total_pages += 1;
+        }
+
+        globals.dvi.cur_s -= 1;
+    }
+
+    if globals.engine.total_pages == 0 {
+        rs_print_nl_bytes(globals, b"No pages of output.");
+        return;
+    }
+
+    if globals.dvi.cur_s == -2 {
+        /* This happens when the DVI gets too big; a message has already been printed */
+        return;
+    }
+
+    rs_dvi_out(globals, POST);
+    rs_dvi_four(globals, globals.engine.last_bop);
+    globals.engine.last_bop = globals.dvi.offset + globals.dvi.ptr - 5;
+    rs_dvi_four(globals, 25400000); /* magic values: conversion ratio for sp */
+    rs_dvi_four(globals, 473628672); /* magic values: conversion ratio for sp */
+    rs_prepare_mag(globals);
+    rs_dvi_four(globals, globals.engine.int_par(IntPar::Mag));
+    rs_dvi_four(globals, globals.engine.max_v);
+    rs_dvi_four(globals, globals.engine.max_h);
+    rs_dvi_out(globals, (globals.engine.max_push / 256) as u8);
+    rs_dvi_out(globals, (globals.engine.max_push % 256) as u8);
+    rs_dvi_out(globals, (globals.engine.total_pages / 256 % 256) as u8);
+    rs_dvi_out(globals, (globals.engine.total_pages % 256) as u8);
+
+    while globals.fonts.font_ptr > FONT_BASE {
+        if globals.fonts.font_used[globals.fonts.font_ptr as usize] {
+            rs_dvi_font_def(globals, globals.fonts.font_ptr as usize);
+        }
+        globals.fonts.font_ptr -= 1;
+    }
+
+    rs_dvi_out(globals, POST_POST);
+    rs_dvi_four(globals, globals.engine.last_bop);
+
+    if globals.engine.semantic_pagination_enabled {
+        rs_dvi_out(globals, SPX_ID_BYTE);
+    } else {
+        rs_dvi_out(globals, XDV_ID_BYTE);
+    }
+
+    let mut k = 4 + (DVI_BUF_SIZE - globals.dvi.ptr) % 4;
+    while k > 0 {
+        rs_dvi_out(globals, 223);
+        k -= 1;
+    }
+
+    if globals.dvi.limit == HALF_BUF {
+        rs_write_to_dvi(globals, HALF_BUF as usize, (DVI_BUF_SIZE - 1) as usize);
+    }
+
+    if globals.dvi.ptr > TEX_INFINITY - globals.dvi.offset {
+        globals.dvi.cur_s = -2;
+        // TODO: fatal_error("dvi length exceeds 0x7FFFFFFF");
+        todo!();
+    }
+
+    if globals.dvi.ptr > 0 {
+        rs_write_to_dvi(globals, 0, (globals.dvi.ptr - 1) as usize);
+    }
+
+    let k = globals
+        .dvi
+        .file
+        .map(|file| globals.state.output_close(file))
+        .unwrap_or(false);
+
+    if k {
+        rs_print_nl_bytes(globals, b"Error ");
+        rs_print_int(globals, 1);
+        rs_print_bytes(globals, b" (Operation not permitted) generating output;");
+        rs_print_nl_bytes(globals, b"file ");
+        rs_print(globals, globals.dvi.output_file_name);
+        rs_print_bytes(globals, b" may not be valid.");
+        /* XeTeX adds history = OUTPUT_FAILURE = 4 here; I'm not implementing that. */
+    } else {
+        rs_print_nl_bytes(globals, b"Output written on ");
+        rs_print(globals, globals.dvi.output_file_name);
+        rs_print_bytes(globals, b" (");
+        rs_print_int(globals, globals.engine.total_pages);
+        let follow = if globals.engine.total_pages != 1 {
+            b" pages" as &[_]
+        } else {
+            b" page"
+        };
+        rs_print_bytes(globals, follow);
+        rs_print_bytes(globals, b", ");
+        rs_print_int(globals, globals.dvi.offset + globals.dvi.ptr);
+        rs_print_bytes(globals, b" bytes).");
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn finalize_dvi_file() {
+    Globals::with(|globals| rs_finalize_dvi_file(globals));
 }
